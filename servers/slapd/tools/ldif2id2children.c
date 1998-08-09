8d5c2b41e69bd8802b4730f90a870a9302ba31f2
Kurt Zeilenga <kurt@openldap.org>
1998-08-09 02:33:01 +0000

    LDAPworld P3: SLAPD LDBM Generation with Multiple Backends

--- servers/slapd/tools/ldif2id2children.c
+++ servers/slapd/tools/ldif2id2children.c
@@ -121,7 +121,7 @@ main( int argc, char **argv )
 			fprintf( stderr, "No ldbm database found in config file\n" );
 			exit( 1 );
 		}
-	} else if ( dbnum < 1 || dbnum > nbackends ) {
+	} else if ( dbnum < 0 || dbnum > (nbackends-1) ) {
 		fprintf( stderr, "Database number selected via -n is out of range\n" );
 		fprintf( stderr, "Must be in the range 1 to %d (number of databases in the config file)\n", nbackends );
 		exit( 1 );
