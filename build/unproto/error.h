af40e0cea7192eed0b09dcf069361db2d3f5e07a
Kurt Zeilenga <kurt@openldap.org>
1998-10-21 02:27:59 +0000

    The world is STDC.

--- build/unproto/error.h
+++
@@ -1,6 +0,0 @@
-/* @(#) error.h 1.2 92/01/15 21:53:14 */
-
-extern int errcount;			/* error counter */
-extern void error();			/* default context */
-extern void error_where();		/* user-specified context */
-extern void fatal();			/* fatal error */
