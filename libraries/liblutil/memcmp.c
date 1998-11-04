cedf6b4b5f4424cdd8b102654f6387937060b612
Kurt Zeilenga <kurt@openldap.org>
1998-11-04 18:47:15 +0000

    8-bit memcmp()

---
+++ libraries/liblutil/memcmp.c
@@ -0,0 +1,19 @@
+#include "portable.h"
+
+#include <ac/string.h>
+
+/* 
+ * Memory Compare
+ */
+int
+memcmp(const void *v1, const void *v2, int n) 
+{
+    if (n != 0) {
+		register const unsigned char *s1=v1, *s2=v2;
+        do {
+            if (*s1++ != *s2++)
+                return (*--s1 - *--s2);
+        } while (--n != 0);
+    }
+    return (0);
+} 
