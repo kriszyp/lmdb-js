d4a4a4cd25ee0c8e3e08628347aad2bcbcb4c894
Kurt Zeilenga <kurt@openldap.org>
1998-11-06 22:04:14 +0000

    Add lutil_passwd()

--- include/lutil.h
+++ include/lutil.h
@@ -13,7 +13,7 @@ LDAP_BEGIN_DECL
 LDAP_F int b64_ntop LDAP_P((u_char const *, size_t, char *, size_t));
 LDAP_F int b64_pton LDAP_P((char const *, u_char *, size_t));
 LDAP_F void lutil_detach LDAP_P((int debug, int do_close));
-
+LDAP_F int lutil_passwd LDAP_P((const char *cred, const char *passwd));
 LDAP_END_DECL
 
 #endif /* _LUTIL_H */
