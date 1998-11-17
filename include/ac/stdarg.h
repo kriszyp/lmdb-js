da42acb93a02e988d3793a845b3b084c511528dc
Kurt Zeilenga <kurt@openldap.org>
1998-11-17 19:05:48 +0000

    Update handling of string headers. Update NT port.

--- include/ac/stdarg.h
+++ include/ac/stdarg.h
@@ -3,10 +3,15 @@
 #ifndef _AC_STDARG_H
 #define _AC_STDARG_H 1
 
-#if defined( HAVE_STDARG_H ) && \
-	( defined( __STDC__ ) || defined( _WIN32 ) )
+#if defined( HAVE_STDARG ) || \
+	( defined( HAVE_STDARG_H ) && defined( __STDC__ ) ) 
+
 #	include <stdarg.h>
-#	define HAVE_STDARG 1
+
+#	ifndef HAVE_STDARG
+#		define HAVE_STDARG 1
+#	endif
+
 #else
 #	include <varargs.h>
 #endif
