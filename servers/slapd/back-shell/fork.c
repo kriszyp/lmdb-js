0539e2d4f42dbf0016e9a12381a78205e1a8230c
Kurt Zeilenga <kurt@openldap.org>
1998-12-24 19:03:42 +0000

    Use fork1() instead of fork().  May reduce problems under Solaris THR.

--- servers/slapd/back-shell/fork.c
+++ servers/slapd/back-shell/fork.c
@@ -32,8 +32,20 @@ forkandexec(
 	 *	parent *rfp <- c2p[0] | c2p[1] <- stdout child
 	 */
 
-	switch ( (pid = fork()) ) {
+#ifdef HAVE_THR
+	switch ( (pid = fork1()) )
+#else
+	switch ( (pid = fork()) )
+#endif
+	{
 	case 0:		/* child */
+		/*
+		 * child could deadlock here due to resources locked
+		 * by our parent
+		 *
+		 * If so, configure --without-threads or implement forking
+		 * via a surrogate parent.
+		 */
 		close( p2c[1] );
 		close( c2p[0] );
 		if ( dup2( p2c[0], 0 ) == -1 || dup2( c2p[1], 1 ) == -1 ) {
