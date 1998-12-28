43094a1b33036b1abe266f5013ad971a88609e0d
Kurt Zeilenga <kurt@openldap.org>
1998-12-28 19:58:57 +0000

    Update copyright notices for 1999.

--- build/top.mk
+++ build/top.mk
@@ -1,7 +1,11 @@
 ##
-## Copyright 1998 The OpenLDAP Foundation
-## COPYING RESTRICTIONS APPLY.  See COPYRIGHT File in top level directory
-## of this package for details.
+## Copyright 1998,1999 The OpenLDAP Foundation, Redwood City, California, USA
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms are permitted only
+## as authorized by the OpenLDAP Public License.  A copy of this
+## license is available at http://www.OpenLDAP.org/license.html or
+## in file LICENSE in the top-level directory of the distribution.
 ##
 PACKAGE= @PACKAGE@
 VERSION= @VERSION@
