91363c270e17af396993b4c5b83ecdaf76a3e8f8
Kurt Zeilenga <kurt@openldap.org>
1999-02-10 20:49:38 +0000

    Remove gtk-tool from the Release Engineering branch until updated to support the 1.x API.

--- contrib/gtk-tool/system.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_SYSTEM_H
-#define MY_SYSTEM_M
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <linux/string.h> /* for memcpy.h */
-#endif
--- contrib/gtk-tool/utils.h
+++
@@ -1,4 +0,0 @@
-#ifndef MY_UTIL_H
-#define MY_UTIL_H
-int debug(const char *format,...);
-#endif /* MY_UTIL_H */
