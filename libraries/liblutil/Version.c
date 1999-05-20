19754332345441bed45d8da8a43ef7c2d718a05f
Kurt Zeilenga <kurt@openldap.org>
1999-05-20 19:35:07 +0000

    Deprecated in favor of mkversion.

--- libraries/liblutil/Version.c
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 1998 The LDAP Foundation
- * All rights reserved.
- *
- * This software may be used and distributed as provided under the
- * terms of the OpenLDAP Public License.
- */
-
-static char Version[] = "  liblutil.a v%VERSION% (%WHEN%)\n\t%WHOANDWHERE%\n";
