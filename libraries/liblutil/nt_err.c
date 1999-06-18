7a7f3018fa0d0d58c2699ca86824735982502376
Gary Williams <gwilliams@openldap.org>
1999-06-18 21:27:43 +0000

    fix flag on formatMessage to not allocate buffer

--- libraries/liblutil/nt_err.c
+++ libraries/liblutil/nt_err.c
@@ -8,7 +8,7 @@ char *GetErrorString( int err )
 	static char msgBuf[1024];
 
 	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		FORMAT_MESSAGE_FROM_SYSTEM,
 		NULL,
 		err, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		msgBuf, 1024, NULL );
