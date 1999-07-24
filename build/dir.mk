61d0ffd135b7f55b3037146442084d48ddbdd20d
Howard Chu <hyc@openldap.org>
1999-07-24 08:25:48 +0000

    Minor changes to support parallel make: Eliminate for-loops for building subdirectories; add explicit dependencies for subdirs that need them.

--- build/dir.mk
+++ build/dir.mk
@@ -6,45 +6,12 @@
 ## Makes subdirectories
 ##
 
+all-common install-common clean-common veryclean-common depend-common: FORCE
+	@T=`echo $@ | cut -d- -f1`; echo "Making $$T in `$(PWD)`"; \
+	 $(MAKE) $(MFLAGS) $(SUBDIRS) TARG=$$T
 
-all-common: FORCE
-	@echo "Making all in `$(PWD)`"
-	@for i in $(SUBDIRS) $(ALLDIRS); do 		\
-		echo "  Entering subdirectory $$i";		\
-		( cd $$i; $(MAKE) $(MFLAGS) all );		\
-		echo " ";								\
-	done
-
-install-common: FORCE
-	@echo "Making install in `$(PWD)`"
-	@for i in $(SUBDIRS) $(INSTALLDIRS); do 	\
-		echo "  Entering subdirectory $$i";		\
-		( cd $$i; $(MAKE) $(MFLAGS) install );	\
-		echo " ";								\
-	done
-
-clean-common: FORCE
-	@echo "Making clean in `$(PWD)`"
-	@for i in $(SUBDIRS) $(CLEANDIRS); do		\
-		echo "  Entering subdirectory $$i";		\
-		( cd $$i; $(MAKE) $(MFLAGS) clean );	\
-		echo " ";								\
-	done
-
-veryclean-common: FORCE
-	@echo "Making veryclean in `$(PWD)`"
-	@for i in $(SUBDIRS) $(CLEANDIRS); do		\
-		echo "  Entering subdirectory $$i";		\
-		( cd $$i; $(MAKE) $(MFLAGS) veryclean );	\
-		echo " ";								\
-	done
-
-depend-common: FORCE
-	@echo "Making depend in `$(PWD)`"
-	@for i in $(SUBDIRS) $(DEPENDDIRS); do		\
-		echo "  Entering subdirectory $$i";		\
-		( cd $$i; $(MAKE) $(MFLAGS) depend );	\
-		echo " ";								\
-	done
+$(SUBDIRS): FORCE
+	@echo "  Entering subdirectory $@"; cd $@; $(MAKE) $(MFLAGS) $(TARG); \
+	echo ""
 
 Makefile: $(top_srcdir)/build/dir.mk
