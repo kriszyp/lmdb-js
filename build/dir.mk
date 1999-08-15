250955490449398626d5ad6c840edc562192c981
Kurt Zeilenga <kurt@openldap.org>
1999-08-15 22:09:29 +0000

    Backout last two commits.  Merged rule breaks specific behavior of each rule!

--- build/dir.mk
+++ build/dir.mk
@@ -6,13 +6,45 @@
 ## Makes subdirectories
 ##
 
-all-common install-common clean-common veryclean-common depend-common: FORCE
-	@t=`echo $@ | cut -d- -f1`; \
-	echo "Making $$t in `$(PWD)`"; \
-	for i in $(SUBDIRS); do \
-		echo "  Entering subdirectory $$i to execute:"; \
-		echo "    $(MAKE) $(MFLAGS) $$t"; \
-		( cd $$i; $(MAKE) $(MFLAGS) $$t ); \
+
+all-common: FORCE
+	@echo "Making all in `$(PWD)`"
+	@for i in $(SUBDIRS) $(ALLDIRS); do 		\
+		echo "  Entering subdirectory $$i";		\
+		( cd $$i; $(MAKE) $(MFLAGS) all );		\
+		echo " ";								\
+	done
+
+install-common: FORCE
+	@echo "Making install in `$(PWD)`"
+	@for i in $(SUBDIRS) $(INSTALLDIRS); do 	\
+		echo "  Entering subdirectory $$i";		\
+		( cd $$i; $(MAKE) $(MFLAGS) install );	\
+		echo " ";								\
+	done
+
+clean-common: FORCE
+	@echo "Making clean in `$(PWD)`"
+	@for i in $(SUBDIRS) $(CLEANDIRS); do		\
+		echo "  Entering subdirectory $$i";		\
+		( cd $$i; $(MAKE) $(MFLAGS) clean );	\
+		echo " ";								\
+	done
+
+veryclean-common: FORCE
+	@echo "Making veryclean in `$(PWD)`"
+	@for i in $(SUBDIRS) $(CLEANDIRS); do		\
+		echo "  Entering subdirectory $$i";		\
+		( cd $$i; $(MAKE) $(MFLAGS) veryclean );	\
+		echo " ";								\
+	done
+
+depend-common: FORCE
+	@echo "Making depend in `$(PWD)`"
+	@for i in $(SUBDIRS) $(DEPENDDIRS); do		\
+		echo "  Entering subdirectory $$i";		\
+		( cd $$i; $(MAKE) $(MFLAGS) depend );	\
+		echo " ";								\
 	done
 
 Makefile: $(top_srcdir)/build/dir.mk
