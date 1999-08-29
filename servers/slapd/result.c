580447be5be4b795808de3ec4175c3f9af99f0f8
Kurt Zeilenga <kurt@openldap.org>
1999-08-29 20:13:33 +0000

    Fix Debug() with too few arguments.

--- servers/slapd/result.c
+++ servers/slapd/result.c
@@ -186,7 +186,7 @@ static long send_ldap_ber(
 		 */
 
 		Debug( LDAP_DEBUG_CONNS, "ber_flush failed errno=%d reason=\"%s\"\n",
-		    err, STRERROR(err) );
+		    err, STRERROR(err), 0 );
 
 		if ( err != EWOULDBLOCK && err != EAGAIN ) {
 			connection_closing( conn );
