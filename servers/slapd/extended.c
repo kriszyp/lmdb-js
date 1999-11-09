951b275ed6e967b80f72da71c5fb4c8c825851e6
Mark Valence <mrv@openldap.org>
1999-11-09 21:42:45 +0000

    Added support for external extensions (based on moduleload).  All new code is conditional on SLAPD_EXTERNAL_EXTENSIONS.

--- servers/slapd/extended.c
+++ servers/slapd/extended.c
@@ -32,9 +32,35 @@
 
 #include "slap.h"
 
+#ifdef SLAPD_EXTERNAL_EXTENSIONS
+
+typedef struct extensions_cookie_t {
+    Connection	*conn;
+    Operation	*op;
+} extensions_cookie_t;
+
+#define MAX_OID_LENGTH	128
+
+typedef struct extensions_list_t {
+	struct extensions_list_t *next;
+	char *oid;
+	int (*ext_main)(int (*)(), void *, char *reqoid, struct berval *reqdata, char **rspoid, struct berval *rspdata, char **text);
+} extensions_list_t;
+
+extensions_list_t *supp_ext_list = NULL;
+
+#define MAX_SUPP_EXT_TRACKED	64
+int supp_ext_count = 0;
+char *supportedExtensions[MAX_SUPP_EXT_TRACKED] = { NULL };
+
+extensions_list_t *find_extension (extensions_list_t *list, char *oid);
+int extensions_callback (extensions_cookie_t *cookie, int msg, int arg, void *argp);
+#else
+
 char *supportedExtensions[] = {
 	NULL
 };
+#endif
 
 
 int
@@ -48,6 +74,12 @@ do_extended(
 	struct berval reqdata;
 	ber_tag_t tag;
 	ber_len_t len;
+#ifdef SLAPD_EXTERNAL_EXTENSIONS
+	extensions_list_t *ext;
+	char *rspoid, *text;
+	struct berval rspdata;
+	extensions_cookie_t cookie;
+#endif
 
 	Debug( LDAP_DEBUG_TRACE, "do_extended\n", 0, 0, 0 );
 
@@ -71,7 +103,12 @@ do_extended(
 		goto done;
 	}
 
-	if( !charray_inlist( supportedExtensions, reqoid ) ) {
+#ifdef SLAPD_EXTERNAL_EXTENSIONS
+	if( !(ext = find_extension(supp_ext_list, reqoid)) )
+#else
+	if( !charray_inlist( supportedExtensions, reqoid ) )
+#endif
+	{
 		Debug( LDAP_DEBUG_ANY, "do_extended: unsupported operation \"%s\"\n",
 			reqoid, 0 ,0 );
 		send_ldap_result( conn, op, rc = LDAP_PROTOCOL_ERROR,
@@ -98,8 +135,29 @@ do_extended(
 
 	Debug( LDAP_DEBUG_ARGS, "do_extended: oid \"%s\"\n", reqoid, 0 ,0 );
 
+#ifdef SLAPD_EXTERNAL_EXTENSIONS
+	cookie.conn = conn;
+	cookie.op = op;
+	rspoid = NULL;
+	rspdata.bv_len = 0;
+	rspdata.bv_val = NULL;
+	text = NULL;
+	rc = (ext->ext_main)(extensions_callback, &cookie, reqoid, &reqdata, &rspoid, &rspdata, &text);
+
+	send_ldap_extended(conn, op, rc, NULL, text, rspoid, rspdata.bv_val ? &rspdata : NULL);
+
+	if (rspoid != NULL)
+		free(rspoid);
+	if ( rspdata.bv_val != NULL )
+		free(rspdata.bv_val);
+	if ( text != NULL )
+		free(text);
+
+#else
 	send_ldap_result( conn, op, rc = LDAP_PROTOCOL_ERROR,
 		NULL, "unsupported extended operation", NULL, NULL );
+
+#endif
 
 done:
 	if ( reqoid != NULL ) {
@@ -111,3 +169,127 @@ done:
 
 	return rc;
 }
+
+#ifdef SLAPD_EXTERNAL_EXTENSIONS
+
+int
+load_extension (
+	const void *module,
+	const char *file_name
+)
+{
+	extensions_list_t *ext;
+	int (*ext_getoid)(int index, char *oid, int blen);
+	int rc;
+
+	ext = ch_calloc(1, sizeof(extensions_list_t));
+	if (ext == NULL)
+		return(-1);
+
+	ext->oid = ch_malloc(MAX_OID_LENGTH);
+	if (ext->oid == NULL) {
+		free(ext);
+		return(-1);
+	}
+
+	ext->ext_main = module_resolve(module, "ext_main");
+	if (ext->ext_main == NULL) {
+		free(ext->oid);
+		free(ext);
+		return(-1);
+	}
+
+	ext_getoid = module_resolve(module, "ext_getoid");
+	if (ext_getoid == NULL) {
+		free(ext->oid);
+		free(ext);
+		return(-1);
+	}
+	rc = (ext_getoid)(0, ext->oid, MAX_OID_LENGTH);
+	if (rc != 0) {
+		free(ext->oid);
+		free(ext);
+		return(rc);
+	}
+	if (*ext->oid == 0) {
+		free(ext->oid);
+		free(ext);
+		return(-1);
+	}
+
+	/* supportedExtensions must be maintained for the root DSE.
+	 * Unfortunately, this global var is declared extern char *[],
+	 * which means it cannot grow dynamically.  So, for now it is
+	 * a char *[n], and only (n-1) oids are tracked.  In the off
+	 * chance that this is too few, the extensions will still be
+	 * loaded, but not reported in root DSE info.  To increase
+	 * the maximum, change MAX_SUPP_EXT_TRACKED and recompile or
+	 * fix root_dse.c to use something other than a static array.
+	 */
+	if (supp_ext_count < (MAX_SUPP_EXT_TRACKED - 1)) {
+		supportedExtensions[supp_ext_count++] = ch_strdup(ext->oid);
+		supportedExtensions[supp_ext_count] = NULL;
+	}
+
+	ext->next = supp_ext_list;
+	supp_ext_list = ext;
+	return(0);
+}
+
+extensions_list_t *
+find_extension (extensions_list_t *list, char *oid)
+{
+	extensions_list_t *ext;
+
+	for (ext = list; ext; ext = ext->next) {
+		if (strcmp(ext->oid, oid) == 0)
+			return(ext);
+	}
+	return(NULL);
+}
+
+int
+extensions_callback (extensions_cookie_t *cookie, int msg, int arg, void *argp)
+{
+	if (cookie == NULL)
+		return(-1);
+
+	if (argp == NULL)
+		return(-1);
+
+	switch (msg) {
+	case 0:		/* SLAPD_EXT_GETVERSION */
+		*(int *)argp = 1;
+		return(0);
+
+	case 1:		/* SLAPD_EXT_GETPROTO */
+		*(int *)argp = cookie->op->o_protocol;
+		return(0);
+	
+	case 2:		/* SLAPD_EXT_GETAUTH */
+		*(int *)argp = cookie->op->o_authtype;
+		return(0);
+	
+	case 3:		/* SLAPD_EXT_GETDN */
+		*(char **)argp = cookie->op->o_dn;
+		return(0);
+	
+	case 4:		/* SLAPD_EXT_GETCLIENT */
+		if (cookie->conn->c_peer_domain != NULL && *cookie->conn->c_peer_domain != 0) {
+			*(char **)argp = cookie->conn->c_peer_domain;
+			return(0);
+		}
+		if (cookie->conn->c_peer_name != NULL && *cookie->conn->c_peer_name != 0) {
+			*(char **)argp = cookie->conn->c_peer_name;
+			return(0);
+		}
+		break;
+	
+	default:
+		break;
+	}
+	return(-1);
+}
+
+#endif
+
