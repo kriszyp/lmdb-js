c044b2a667a23af8ed0a2469b73b18324f7427b8
Kurt Zeilenga <kurt@openldap.org>
2000-03-11 00:58:10 +0000

    Remove race condition noted in ITS#216.

--- libraries/libldap_r/thr_nt.c
+++ libraries/libldap_r/thr_nt.c
@@ -96,7 +96,7 @@ ldap_pvt_thread_cond_wait( ldap_pvt_thread_cond_t *cond,
 			  ldap_pvt_thread_mutex_t *mutex )
 {
 	ReleaseMutex( *mutex );
-	WaitForSingleObject( *cond, INFINITE );
+	SignalObjectAndWait( *mutex, *cond, INFINITE, FALSE );
 	WaitForSingleObject( *mutex, INFINITE );
 	return( 0 );
 }
