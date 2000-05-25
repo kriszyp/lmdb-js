1ae20e9b0c3ee7ed6445114aadac57ac3aeb90a4
Kurt Zeilenga <kurt@openldap.org>
2000-05-25 02:09:59 +0000

    Move extern schema_init_done outside of #ifdef SLAPD_SCHEMA_NOT_COMPAT

--- servers/slapd/proto-slap.h
+++ servers/slapd/proto-slap.h
@@ -10,9 +10,9 @@
 
 LDAP_BEGIN_DECL
 
+LIBSLAPD_F( int ) schema_init_done;
 #ifdef SLAPD_SCHEMA_NOT_COMPAT
 LIBSLAPD_F( struct slap_internal_schema ) slap_schema;
-LIBSLAPD_F( int ) schema_init_done;
 
 LIBSLAPD_F (int) slap_str2ad LDAP_P((
 	const char *,
