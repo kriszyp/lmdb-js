3888bac4bf4d8515c3433ed51b1dd461f100bf8c
Mark Valence <mrv@openldap.org>
2000-06-15 08:34:48 +0000

    Added Generalized Time matching and ordering (it's just caseExactIA5Ma tch).  Set OpenLDAP Experimental ACI syntax validation to IA5StringVal idate for now, to facilitate testing.

--- servers/slapd/schema_init.c
+++ servers/slapd/schema_init.c
@@ -1308,7 +1308,7 @@ struct syntax_defs_rec syntax_defs[] = {
 
 	/* OpenLDAP Experimental Syntaxes */
 	{"( 1.3.6.1.4.1.4203.666.2.1 DESC 'OpenLDAP Experimental ACI' )",
-		0, NULL, NULL, NULL},
+		0, IA5StringValidate /* THIS WILL CHANGE FOR NEW ACI SYNTAX */, NULL, NULL},
 	{"( 1.3.6.1.4.1.4203.666.2.2 DESC 'OpenLDAP authPassword' )",
 		0, NULL, NULL, NULL},
 	{"( 1.3.6.1.4.1.4203.666.2.3 DESC 'OpenLDAP void' " X_HIDE ")" ,
@@ -1378,8 +1378,8 @@ struct mrule_defs_rec {
 #define presentationAddressMatch NULL
 #define uniqueMemberMatch NULL
 #define protocolInformationMatch NULL
-#define generalizedTimeMatch NULL
-#define generalizedTimeOrderingMatch NULL
+#define generalizedTimeMatch caseExactIA5Match
+#define generalizedTimeOrderingMatch caseExactIA5Match
 #define integerFirstComponentMatch NULL
 #define objectIdentifierFirstComponentMatch NULL
 
