bfff7ce1f5cfe651274882e39a250ceb0a7e3d40
Kurt Zeilenga <kurt@openldap.org>
2000-09-07 23:31:59 +0000

    Bandaid ucdata build problems

--- include/ldap_pvt.h
+++ include/ldap_pvt.h
@@ -172,80 +172,9 @@ LDAP_F (int) ldap_pvt_tls_start LDAP_P(( struct ldap *ld, Sockbuf *sb, void *ctx
 LDAP_F (int) ldap_pvt_tls_get_option LDAP_P(( struct ldapoptions *lo, int option, void *arg ));
 LDAP_F (int) ldap_pvt_tls_set_option LDAP_P(( struct ldapoptions *lo, int option, void *arg ));
 
-/*  
- * UTF-8 (in utf-8.c)
- */
-
-typedef ber_int_t ldap_ucs4_t;
-#define LDAP_UCS4_INVALID (0x80000000U)
-
-typedef short ldap_ucs2_t;
-
-/* UCDATA uses UCS-2 passed in an unsigned long */
-typedef unsigned long ldap_unicode_t;
-
-/* conversion routines  */
-LDAP_F( ldap_ucs4_t ) ldap_utf8_to_ucs4( const char * p );
-LDAP_F( int ) ldap_ucs4_to_utf8( ldap_ucs4_t c, char *buf );
-
-#define ldap_utf8_to_unicode( p ) ldap_utf8_to_ucs4((p))
-#define ldap_unicode_to_utf8( c, buf ) ldap_ucs4_to_ucs4((c),(buf))
-
-/* returns the number of bytes in the UTF-8 string */
-LDAP_F (ber_len_t) ldap_utf8_bytes( const char * );
-/* returns the number of UTF-8 characters in the string */
-LDAP_F (ber_len_t) ldap_utf8_chars( const char * );
-/* returns the length (in bytes) of the UTF-8 character */
-LDAP_F (int) ldap_utf8_offset( const char * );
-/* returns the length (in bytes) indicated by the UTF-8 character */
-LDAP_F (int) ldap_utf8_charlen( const char * );
-/* copies a UTF-8 character and returning number of bytes copied */
-LDAP_F (int) ldap_utf8_copy( char *, const char *);
-
-/* returns pointer of next UTF-8 character in string */
-LDAP_F (char*) ldap_utf8_next( const char * );
-/* returns pointer of previous UTF-8 character in string */
-LDAP_F (char*) ldap_utf8_prev( const char * );
-
-/* primitive ctype routines -- not aware of non-ascii characters */
-LDAP_F (int) ldap_utf8_isascii( const char * );
-LDAP_F (int) ldap_utf8_isalpha( const char * );
-LDAP_F (int) ldap_utf8_isalnum( const char * );
-LDAP_F (int) ldap_utf8_isdigit( const char * );
-LDAP_F (int) ldap_utf8_isxdigit( const char * );
-LDAP_F (int) ldap_utf8_isspace( const char * );
-
-/* span characters not in set, return bytes spanned */
-LDAP_F (ber_len_t) ldap_utf8_strcspn( const char* str, const char *set);
-/* span characters in set, return bytes spanned */
-LDAP_F (ber_len_t) ldap_utf8_strspn( const char* str, const char *set);
-/* return first occurance of character in string */
-LDAP_F (char *) ldap_utf8_strchr( const char* str, const char *chr);
-/* return first character of set in string */
-LDAP_F (char *) ldap_utf8_strpbrk( const char* str, const char *set);
-/* reentrant tokenizer */
-LDAP_F (char*) ldap_utf8_strtok( char* sp, const char* sep, char **last);
-
-/* Optimizations */
-#define LDAP_UTF8_ISASCII(p) ( * (const unsigned char *) (p) < 0x100 )
-#define LDAP_UTF8_CHARLEN(p) ( LDAP_UTF8_ISASCII(p) \
-	? 1 : ldap_utf8_charlen((p)) )
-#define LDAP_UTF8_OFFSET(p) ( LDAP_UTF8_ISASCII(p) \
-	? 1 : ldap_utf8_offset((p)) )
-
-#define LDAP_UTF8_COPY(d,s) (	LDAP_UTF8_ISASCII(s) \
-	? (*(d) = *(s), 1) : ldap_utf8_copy((d),(s)) )
-
-#define LDAP_UTF8_NEXT(p) (	LDAP_UTF8_ISASCII(p) \
-	? (char *)(p)+1 : ldap_utf8_next((p)) )
-
-#define LDAP_UTF8_INCR(p) ((p) = LDAP_UTF8_NEXT(p))
-
-/* For symmetry */
-#define LDAP_UTF8_PREV(p) (ldap_utf8_prev((p)))
-#define LDAP_UTF8_DECR(p) ((p)=LDAP_UTF8_PREV((p)))
-
 LDAP_END_DECL
+
+#include "ldap_pvt_uc.h"
 
 #endif
 
