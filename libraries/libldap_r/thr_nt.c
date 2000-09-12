c78d5f5e7f718f07851f79a7cd2ffc023675b54f
Kurt Zeilenga <kurt@openldap.org>
2000-09-12 18:18:27 +0000

    ITS#697: Fix cond_wait

--- libraries/libldap_r/thr_nt.c
+++ libraries/libldap_r/thr_nt.c
@@ -95,7 +95,6 @@ int
 ldap_pvt_thread_cond_wait( ldap_pvt_thread_cond_t *cond, 
 	ldap_pvt_thread_mutex_t *mutex )
 {
-	ReleaseMutex( *mutex );
 	SignalObjectAndWait( *mutex, *cond, INFINITE, FALSE );
 	WaitForSingleObject( *mutex, INFINITE );
 	return( 0 );
