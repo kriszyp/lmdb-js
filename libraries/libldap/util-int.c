6860b136ff86e5a1eaee3688678e8c85a6dd1cf8
Ben Collins <bcollins@openldap.org>
2000-09-15 03:33:05 +0000

    don't define NEED_COPY_HOSTENT in the decleration

--- libraries/libldap/util-int.c
+++ libraries/libldap/util-int.c
@@ -98,7 +98,6 @@ char *ldap_pvt_ctime( const time_t *tp, char *buf )
 static char *safe_realloc( char **buf, int len );
 
 #if !defined(HAVE_GETHOSTBYNAME_R) && defined(LDAP_R_COMPILE)
-# define NEED_COPY_HOSTENT
 static int copy_hostent( struct hostent *res, char **buf, struct hostent * src );
 #endif
 
