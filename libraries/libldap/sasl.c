bad43369e186dca235d0bae4a3753348ce704395
Kurt Zeilenga <kurt@openldap.org>
2000-09-15 18:12:56 +0000

    Don't send empty cred

--- libraries/libldap/sasl.c
+++ libraries/libldap/sasl.c
@@ -103,7 +103,7 @@ ldap_sasl_bind(
 			ld->ld_version, dn, LDAP_AUTH_SIMPLE,
 			cred );
 		
-	} else if ( cred == NULL ) {
+	} else if ( cred == NULL || !cred->bv_len ) {
 		/* SASL bind w/o creditials */
 		rc = ber_printf( ber, "{it{ist{sN}N}" /*}*/,
 			++ld->ld_msgid, LDAP_REQ_BIND,
