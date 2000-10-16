820965a412c60622175516d80e62269b77e402d0
Kurt Zeilenga <kurt@openldap.org>
2000-10-16 20:32:39 +0000

    ITS#839: getaddrinfo portability fix from Gabar Gombas

--- servers/slapd/daemon.c
+++ servers/slapd/daemon.c
@@ -291,6 +291,10 @@ static int slap_get_listener_addresses(
 			*sap = NULL;
 			break;
 		}
+		if (*sap != NULL) {
+			(*sap)->sa_family = sai->ai_family;
+			(*sap)->sa_len = sai->ai_addrlen;
+		}
 		sap++;
 	} while ((sai = sai->ai_next) != NULL);
 
