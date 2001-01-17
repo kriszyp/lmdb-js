fc21ab1796fe2b4201ac70fd29128b720e56d4a7
Kurt Zeilenga <kurt@openldap.org>
2001-01-17 07:13:14 +0000

    Update 'altered' state

--- servers/slapd/back-ldap/unbind.c
+++ servers/slapd/back-ldap/unbind.c
@@ -1,7 +1,7 @@
 /* unbind.c - ldap backend unbind function */
 /* $OpenLDAP$ */
 /*
- * Copyright 1998-2000 The OpenLDAP Foundation, All Rights Reserved.
+ * Copyright 1998-1999 The OpenLDAP Foundation, All Rights Reserved.
  * COPYING RESTRICTIONS APPLY, see COPYRIGHT file
  */
 /* This is an altered version */
