ec87e9e30f7cde21ac4b5ab0a8b01225e98fb8bc
Kurt Zeilenga <kurt@openldap.org>
2001-01-22 08:06:48 +0000

    Default objectClass kind should be STRUCTURAL

--- libraries/libldap/schema.c
+++ libraries/libldap/schema.c
@@ -1977,6 +1977,7 @@ ldap_str2objectclass( const char * s, int * code, const char ** errp, const int 
 		*code = LDAP_SCHERR_OUTOFMEM;
 		return NULL;
 	}
+	oc->oc_kind = LDAP_SCHEMA_STRUCTURAL;
 
 	kind = get_token(&ss,&sval);
 	if ( kind != TK_LEFTPAREN ) {
