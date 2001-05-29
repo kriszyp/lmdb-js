be9a50af258c16083245db489aca5676c429f606
Kurt Zeilenga <kurt@openldap.org>
2001-05-29 03:29:53 +0000

    Update copyright (including with or without modification clarification)

--- include/ldap_utf8.h
+++ include/ldap_utf8.h
@@ -1,12 +1,13 @@
 /* $OpenLDAP$ */
 /*
- * Copyright 2000-2001 The OpenLDAP Foundation, Redwood City, California, USA
+ * Copyright 1998-2001 The OpenLDAP Foundation, Redwood City, California, USA
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted only
- * as authorized by the OpenLDAP Public License.  A copy of this
- * license is available at http://www.OpenLDAP.org/license.html or
- * in file LICENSE in the top-level directory of the distribution.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the OpenLDAP
+ * Public License.  A copy of this license is available at
+ * http://www.OpenLDAP.org/license.html or in file LICENSE in the
+ * top-level directory of the distribution.
  */
 /* $Novell: /ldap/src/cldap/include/ldap_utf8.h,v 1.3 2000/12/04 20:23:20 dsteck Exp $ 
 /******************************************************************************
