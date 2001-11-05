b6ff5c4d50218496492bbadad879dba88b7054e5
Pierangelo Masarati <ando@openldap.org>
2001-11-05 08:52:01 +0000

    cleanup

--- tests/scripts/subfilter.sh
+++ tests/scripts/subfilter.sh
@@ -1,10 +1,6 @@
 #! /bin/sh
 #
-# Strip entries that belong to subtree $2 (if any)
+# Strip entries that belong to subtree $1
 #
-if test $# == 0 ; then
-	exit 1
-else
-	awk "/^dn:/&&!/$1\$/ {while (\$1!=\"\") {print \$0;getline} print \"\"}"
-fi
+awk "/^dn:/&&!/$1\$/ {while (\$1!=\"\") {print \$0;getline} print \"\"}"
 
