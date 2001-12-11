bfe6df6b209dbe163de35d85ef3e48a6c2771633
Howard Chu <hyc@openldap.org>
2001-12-11 14:23:05 +0000

    Don't signal error if reindexing an already existing dn2id index

--- servers/slapd/back-ldbm/tools.c
+++ servers/slapd/back-ldbm/tools.c
@@ -290,10 +290,8 @@ int ldbm_tool_entry_reindex(
 		id, e->e_dn, 0 );
 #endif
 
-	rc = dn2id_add( be, e->e_ndn, e->e_id );
-	if( rc == 0 ) {
-		rc = index_entry_add( be, e, e->e_attrs );
-	}
+	dn2id_add( be, e->e_ndn, e->e_id );
+	rc = index_entry_add( be, e, e->e_attrs );
 
 	entry_free( e );
 
