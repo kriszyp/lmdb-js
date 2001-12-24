e544300b05a1651819e94990a81b7896207bc05f
Pierangelo Masarati <ando@openldap.org>
2001-12-24 18:39:41 +0000

    back out previous change

--- servers/slapd/back-ldbm/dn2id.c
+++ servers/slapd/back-ldbm/dn2id.c
@@ -86,7 +86,6 @@ dn2id_add(
 	}
 
 	while ( rc != -1 && pdn != NULL ) {
-		/* FIXME: dn_parent returns NULL if dn is suffix of be */
 		if ( be_issuffix( be, pdn ))
 			break;
 
@@ -328,7 +327,6 @@ dn2id_delete(
 	}
 
 	while ( pdn != NULL ) {
-		/* FIXME: dn_parent returns NULL if dn is suffix of be */
 		if ( be_issuffix( be, pdn ))
 			break;
 
