987339b71d3c623d8ffdc4451d013966699c57e5
Howard Chu <hyc@openldap.org>
2002-06-06 11:11:54 +0000

    ITS#1744 silence warning

--- libraries/libldap_r/thr_nt.c
+++ libraries/libldap_r/thr_nt.c
@@ -38,7 +38,7 @@ ldap_pvt_thread_create( ldap_pvt_thread_t * thread,
 	unsigned long tid;
 	HANDLE thd;
 
-	thd = _beginthreadex( NULL, 0,
+	thd = (HANDLE) _beginthreadex( NULL, 0,
 		(LPTHREAD_START_ROUTINE) start_routine, arg,
 		0, &tid );
 
