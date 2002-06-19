5fcd6b41e3bafdfc2d347829d5c98b293888db9c
Kurt Zeilenga <kurt@openldap.org>
2002-06-19 18:15:20 +0000

    Use #ifdef, not #if, to check SHUT_RDWR

--- include/ac/socket.h
+++ include/ac/socket.h
@@ -81,7 +81,7 @@
 #	define ioctl_t				u_long
 #	define AC_SOCKET_INVALID	((unsigned int) ~0)
 
-#	if SD_BOTH
+#	ifdef SD_BOTH
 #		define tcp_close( s )	(shutdown( s, SD_BOTH ), closesocket( s ))
 #	else
 #		define tcp_close( s )		closesocket( s )
@@ -127,7 +127,7 @@ LBER_F( char * ) ber_pvt_wsa_err2string LDAP_P((int));
 #	define tcp_read( s, buf, len)	read( s, buf, len )
 #	define tcp_write( s, buf, len)	write( s, buf, len )
 
-#	if SHUT_RDWR
+#	ifdef SHUT_RDWR
 #		define tcp_close( s )	(shutdown( s, SHUT_RDWR ), close( s ))
 #	else
 #		define tcp_close( s )	close( s )
