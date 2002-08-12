c9565e7530e71b381c1c31a3bf3fb8a8497cf111
Kurt Zeilenga <kurt@openldap.org>
2002-08-12 21:49:27 +0000

    Rewrite master/slave ports

--- tests/scripts/conf.sh
+++ tests/scripts/conf.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 # $OpenLDAP$
+# . $SRCDIR/scripts/defines.sh
 if [ x"$MONITORDB" = x"yes" ] ; then
 	MON=monitor
 else
 	MON=nomonitor
 fi
-sed -e "s/@BACKEND@/$BACKEND/" -e "s/^#$BACKEND#//"  -e "s/^#$MON#//"
+sed -e "s/@BACKEND@/$BACKEND/" -e "s/^#$BACKEND#//"  -e "s/^#$MON#//" \
+	-e "s/@PORT@/$PORT/" -e "s/@SLAVEPORT@/$SLAVEPORT/"
