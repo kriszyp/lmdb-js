00003ac53910347f15c53370b4d320606c69afe1
Kurt Zeilenga <kurt@openldap.org>
2002-08-27 01:35:43 +0000

    Fix comment

--- servers/slapd/schema_check.c
+++ servers/slapd/schema_check.c
@@ -99,7 +99,7 @@ entry_schema_check(
 	/* it's a REALLY bad idea to disable schema checks */
 	if( !global_schemacheck ) return LDAP_SUCCESS;
 
-	/* find the object class attribute - could error out here */
+	/* find the structural object class attribute */
 	asc = attr_find( e->e_attrs, ad_structuralObjectClass );
 	if ( asc == NULL ) {
 #ifdef NEW_LOGGING
