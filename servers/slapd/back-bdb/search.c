e6fdfd4ceebaf21a83b4e409d1b015a505a4ca09
Kurt Zeilenga <kurt@openldap.org>
2002-09-09 23:25:22 +0000

    only optimize objectClass filters when (objectClass=*) is presented... avoids (&(objectClass=*)(objectClass=foo)) problems.

--- servers/slapd/back-bdb/search.c
+++ servers/slapd/back-bdb/search.c
@@ -731,16 +731,19 @@ static int oc_filter(
 
 	switch(f->f_choice) {
 	case LDAP_FILTER_PRESENT:
-		if (f->f_desc == slap_schema.si_ad_objectClass)
+		if (f->f_desc == slap_schema.si_ad_objectClass) {
 			rc = 1;
+		}
 		break;
 
 	case LDAP_FILTER_AND:
 	case LDAP_FILTER_OR:
 		cur++;
-		for (f=f->f_and; f; f=f->f_next)
-			rc |= oc_filter(f, cur, max);
+		for (f=f->f_and; f; f=f->f_next) {
+			(void) oc_filter(f, cur, max);
+		}
 		break;
+
 	default:
 		break;
 	}
