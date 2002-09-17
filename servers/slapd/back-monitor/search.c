c536b42a7e6b022e388d3e2ad301cffadbdfc7ea
Kurt Zeilenga <kurt@openldap.org>
2002-09-17 04:09:53 +0000

    Fix search return code

--- servers/slapd/back-monitor/search.c
+++ servers/slapd/back-monitor/search.c
@@ -187,6 +187,7 @@ monitor_back_search(
 					attrsonly, NULL );
 			nentries = 1;
 		}
+		rc = LDAP_SUCCESS;
 		monitor_cache_release( mi, e );
 		break;
 
@@ -195,7 +196,7 @@ monitor_back_search(
 				attrs, attrsonly,
 				e, 0, &nentries );
 		if ( rc ) {
-			// error
+			rc = LDAP_OTHER;
 		}		
 		
 		break;
@@ -213,14 +214,14 @@ monitor_back_search(
 				attrs, attrsonly,
 				e, 1, &nentries );
 		if ( rc ) {
-			// error
+			rc = LDAP_OTHER;
 		}
 
 		break;
 	}
 	
-	send_search_result( conn, op, LDAP_SUCCESS,
+	send_search_result( conn, op, rc,
 			NULL, NULL, NULL, NULL, nentries );
 
-	return( 0 );
+	return( rc == LDAP_SUCCESS ? 0 : 1 );
 }
