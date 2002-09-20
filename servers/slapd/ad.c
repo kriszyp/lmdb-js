d9d58ae02fbbb25a530b56cd651358e1952774bd
Kurt Zeilenga <kurt@openldap.org>
2002-09-20 19:58:25 +0000

    Save "-" for no subtypes

--- servers/slapd/ad.c
+++ servers/slapd/ad.c
@@ -446,14 +446,13 @@ int ad_inlist(
 		oc = attrs->an_oc;
 		if( oc == NULL && attrs->an_name.bv_val ) {
 			switch( attrs->an_name.bv_val[0] ) {
-				case '+':
-				case '-': {
+				case '+': { /* new way */
 					struct berval ocname;
 					ocname.bv_len = attrs->an_name.bv_len - 1;
 					ocname.bv_val = &attrs->an_name.bv_val[1];
 					oc = oc_bvfind( &ocname );
 				} break;
-				default:
+				default: /* old (deprecated) way */
 					oc = oc_bvfind( &attrs->an_name );
 			}
 			attrs->an_oc = oc;
