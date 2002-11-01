24f1a11cdeffe47f93ae4a0c64bc62b61a764866
Howard Chu <hyc@openldap.org>
2002-11-01 02:14:55 +0000

    Fix sasl_server_start invocation, must pass NULL cred when credlen is 0.

--- servers/slapd/sasl.c
+++ servers/slapd/sasl.c
@@ -1495,7 +1495,7 @@ int slap_sasl_bind(
 	if ( !conn->c_sasl_bind_in_progress ) {
 		sc = START( ctx,
 			conn->c_sasl_bind_mech.bv_val,
-			cred->bv_len ? cred->bv_val : "",
+			cred->bv_len ? cred->bv_val : NULL,
 			cred->bv_len,
 			(SASL_CONST char **)&response.bv_val, &reslen, &errstr );
 
