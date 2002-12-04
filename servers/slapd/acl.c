b1798f91600e5a9d24603b089490d32e7921db70
Howard Chu <hyc@openldap.org>
2002-12-04 23:20:22 +0000

    Fix typo in prev commit value_find/_ex

--- servers/slapd/acl.c
+++ servers/slapd/acl.c
@@ -1802,7 +1802,7 @@ aci_mask(
 			at != NULL;
 			at = attrs_find( at->a_next, ad ) )
 		{
-			if (value_find( ad, SLAP_MR_VALUE_NORMALIZED_MATCH, at->a_vals, &bv) == 0 ) {
+			if (value_find_ex( ad, SLAP_MR_VALUE_NORMALIZED_MATCH, at->a_vals, &bv) == 0 ) {
 				rc = 1;
 				break;
 			}
