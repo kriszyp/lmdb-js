d6ed1dd3b3e388ebbf900635ebd01bcc3b1b4e48
Kurt Zeilenga <kurt@openldap.org>
2003-02-10 19:43:11 +0000

    Add ITS# to CHANGES import proxy authz test

--- tests/scripts/conf.sh
+++ tests/scripts/conf.sh
@@ -5,4 +5,8 @@ if [ x"$MONITORDB" = x"yes" ] ; then
 else
 	MON=nomonitor
 fi
-sed -e "s/@BACKEND@/$BACKEND/" -e "s/^#$BACKEND#//"  -e "s/^#$MON#//"
+sed -e "s/@BACKEND@/${BACKEND}/"	\
+	-e "s/^#${BACKEND}#//"			\
+	-e "s/^#${MON}#//"				\
+	-e "s/@PORT@/${PORT}/"			\
+	-e "s/@SLAVEPORT@/${SLAVEPORT}/"
