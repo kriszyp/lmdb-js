8ca394496bc547757219c0cc951bac1a12cc7cd9
Kurt Zeilenga <kurt@openldap.org>
2003-02-14 23:32:30 +0000

    Only install readahead layer when connectionless as streams as streams may block.

--- libraries/libldap/open.c
+++ libraries/libldap/open.c
@@ -286,8 +286,8 @@ ldap_int_open_connection(
 			sasl_host = ldap_host_connected_to( conn->lconn_sb );
 #endif
 			break;
-#ifdef LDAP_CONNECTIONLESS
 
+#ifdef LDAP_CONNECTIONLESS
 		case LDAP_PROTO_UDP:
 			port = srv->lud_port;
 
@@ -312,6 +312,10 @@ ldap_int_open_connection(
 #endif
 			ber_sockbuf_add_io( conn->lconn_sb, &ber_sockbuf_io_udp,
 				LBER_SBIOD_LEVEL_PROVIDER, NULL );
+
+			ber_sockbuf_add_io( conn->lconn_sb, &ber_sockbuf_io_readahead,
+				LBER_SBIOD_LEVEL_PROVIDER, NULL );
+
 			break;
 #endif
 		case LDAP_PROTO_IPC:
@@ -336,9 +340,6 @@ ldap_int_open_connection(
 			return -1;
 			break;
 	}
-
-	ber_sockbuf_add_io( conn->lconn_sb, &ber_sockbuf_io_readahead,
-		LBER_SBIOD_LEVEL_PROVIDER, NULL );
 
 #ifdef LDAP_DEBUG
 	ber_sockbuf_add_io( conn->lconn_sb, &ber_sockbuf_io_debug,
