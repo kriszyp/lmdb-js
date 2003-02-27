ca0ee4d91d349ef01c23897922b57bf43342653c
Pierangelo Masarati <ando@openldap.org>
2003-02-27 22:41:13 +0000

    another try at fixing test011

--- tests/scripts/subfilter.sh
+++ tests/scripts/subfilter.sh
@@ -2,5 +2,5 @@
 #
 # Strip entries that belong to subtree $1
 #
-awk "/^dn:/&&!/$1\$/ {while (\$1!=\"\") {print \$0;getline} print \"\"}"
+awk '/^dn:/ && !/'"$1"'$/ {while ($0 != "") {print $0; getline} print ""}'
 
