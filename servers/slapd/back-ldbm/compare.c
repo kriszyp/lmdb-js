338355d30ba66867986d6df9d6e1f2f1bcef186a
Kurt Zeilenga <kurt@openldap.org>
2003-03-16 01:39:39 +0000

    SLAP_NVALUES: value_find v value_find_ex

--- servers/slapd/back-ldbm/compare.c
+++ servers/slapd/back-ldbm/compare.c
@@ -103,7 +103,16 @@ ldbm_back_compare(
 	{
 		rc = LDAP_COMPARE_FALSE;
 
-		if ( value_find( ava->aa_desc, a->a_vals, &ava->aa_value ) == 0 ) {
+#ifdef SLAP_NVALUES
+		if ( value_find_ex( ava->aa_desc,
+			SLAP_MR_ATTRIBUTE_VALUE_NORMALIZED_MATCH |
+				SLAP_MR_ASSERTED_VALUE_NORMALIZED_MATCH,
+			a->a_nvals ? a->a_nvals : a->a_vals,
+			&ava->aa_value ) == 0 )
+#else
+		if ( value_find( ava->aa_desc, a->a_vals, &ava->aa_value ) == 0 )
+#endif
+		{
 			rc = LDAP_COMPARE_TRUE;
 			break;
 		}
