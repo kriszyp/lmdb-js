c086b9aec9518a672749f183b765ad8812c4db19
Howard Chu <hyc@openldap.org>
2003-03-18 22:50:07 +0000

    New version.h to replace mkversion

---
+++ build/version.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 1998-2003 The OpenLDAP Foundation
+ * COPYING RESTRICTIONS APPLY.  See COPYRIGHT file in top level directory
+ * of this package for details.
+ */
+
+static const char copyright[] =
+"Copyright 1998-2003 The OpenLDAP Foundation\n"
+"COPYING RESTRICTIONS APPLY\n";
+
+_OLV_STATIC const char __Version[] =
+"@(#) $" _OLV_PKG ": " _OLV_APP " " _OLV_VER " (" _OLV_DATE " " _OLV_TIME ") $\n\t" _OLV_WHO ":" _OLV_CWD "\n";
+
---
+++ include/version.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 1998-2003 The OpenLDAP Foundation
+ * COPYING RESTRICTIONS APPLY.  See COPYRIGHT file in top level directory
+ * of this package for details.
+ */
+
+#define _OLV_DATE __DATE__
+#define _OLV_TIME __TIME__
+#ifndef _OLV_STATIC
+#define	_OLV_STATIC	static
+#endif
+#include "version0.h"
