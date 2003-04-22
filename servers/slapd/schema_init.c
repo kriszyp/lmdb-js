8e12768eed1f68ac89f939c82dd33be8dcec92a4
Jong Hyuk Choi <jongchoi@openldap.org>
2003-04-22 20:08:37 +0000

    subtreeSpecification syntax change : -binary +validator

--- servers/slapd/schema_init.c
+++ servers/slapd/schema_init.c
@@ -2753,8 +2753,8 @@ static slap_syntax_defs_rec syntax_defs[] = {
 	{"( 1.3.6.1.4.1.1466.115.121.1.44 DESC 'Printable String' )",
 		0, printableStringValidate, NULL},
 	{"( 1.3.6.1.4.1.1466.115.121.1.45 DESC 'SubtreeSpecification' "
-		X_BINARY X_NOT_H_R ")",
-		SLAP_SYNTAX_BINARY|SLAP_SYNTAX_BER, NULL, NULL},
+		/*X_BINARY*/ X_NOT_H_R ")",
+		/*SLAP_SYNTAX_BINARY|*/ SLAP_SYNTAX_BER, berValidate, NULL},
 	{"( 1.3.6.1.4.1.1466.115.121.1.49 DESC 'Supported Algorithm' "
 		X_BINARY X_NOT_H_R ")",
 		SLAP_SYNTAX_BINARY|SLAP_SYNTAX_BER, berValidate, NULL},
