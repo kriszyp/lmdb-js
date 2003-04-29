46e0ea292bdc0bc30eff8b72bdde484d59780942
Hallvard Furuseth <hallvard@openldap.org>
2003-04-29 14:54:22 +0000

    Don't do arithmetic on void pointers.

--- servers/slapd/sl_malloc.c
+++ servers/slapd/sl_malloc.c
@@ -64,11 +64,11 @@ sl_mem_create(
 		sh = ch_malloc( sizeof(struct slab_heap) );
 		sh->h_base = ch_malloc( size );
 		ldap_pvt_thread_pool_setkey( ctx, sl_mem_init, (void *)sh, sl_mem_destroy );
-	} else if ( size > sh->h_end - sh->h_base ) {
+	} else if ( size > (char *) sh->h_end - (char *) sh->h_base ) {
 		sh->h_base = ch_realloc( sh->h_base, size );
 	}
 	sh->h_last = sh->h_base;
-	sh->h_end = sh->h_base + size;
+	sh->h_end = (char *) sh->h_base + size;
 	return sh;
 }
 
@@ -89,7 +89,7 @@ sl_malloc(
 	size += pad + sizeof( ber_len_t );
 	size &= ~pad;
 
-	if (sh->h_last + size >= sh->h_end ) {
+	if ((char *) sh->h_last + size >= (char *) sh->h_end ) {
 #ifdef NEW_LOGGING
 		LDAP_LOG( OPERATION, INFO, 
 			   "sl_malloc of %lu bytes failed, using ch_malloc\n", (long)size, 0,0 );
@@ -101,7 +101,7 @@ sl_malloc(
 	}
 	new = sh->h_last;
 	*new++ = size - sizeof(ber_len_t);
-	sh->h_last += size;
+	sh->h_last = (char *) sh->h_last + size;
 	
 	return( (void *)new );
 }
@@ -162,7 +162,7 @@ sl_realloc( void *ptr, ber_len_t size, void *ctx )
 	/* If reallocing the last block, we can grow it */
 	} else if ( (char *)ptr + p[-1] == sh->h_last ) {
 		new = p;
-		sh->h_last += size - p[-1];
+		sh->h_last = (char *) sh->h_last + size - p[-1];
 		p[-1] = size;
 	
 	/* Nowhere to grow, need to alloc and copy */
