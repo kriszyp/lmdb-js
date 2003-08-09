d8333c0a4b87f1115a0c51fee7fed2dc73761133
Kurt Zeilenga <kurt@openldap.org>
2003-08-09 18:14:04 +0000

    Fix ITS#2672: add_loop_cnt wasn't counting (re2.1 only)

--- servers/slapd/back-bdb/id2entry.c
+++ servers/slapd/back-bdb/id2entry.c
@@ -125,6 +125,8 @@ int bdb_id2entry_rw(
 	}
 
 	if ( rc == 0 ) {
+		int add_loop_cnt = 0;
+
 #ifdef BDB_HIER
 		bdb_fix_dn(be, id, *e);
 #endif
@@ -132,7 +134,6 @@ int bdb_id2entry_rw(
 				&bdb->bi_cache, *e, rw, locker, lock);
 		while ( ret == 1 || ret == -1 ) {
 			Entry *ee;
-			int add_loop_cnt = 0;
 			if ( (*e)->e_private != NULL ) {
 				free ((*e)->e_private);
 			}
