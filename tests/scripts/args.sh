61452ee9c7160f904ea5a865678fc469689457d8
Howard Chu <hyc@openldap.org>
2003-10-10 12:11:22 +0000

    Push invariant args into environment, they were making the "WAIT" parameter too difficult to use.

--- tests/scripts/args.sh
+++ tests/scripts/args.sh
@@ -5,21 +5,6 @@ if test $# -ge 1 ; then
         BACKEND=$1; shift
 fi
 
-BACKENDTYPE=yes
-if test $# -ge 1 ; then
-        BACKENDTYPE=$1; shift
-fi
-
-MONITORDB=no
-if test $# -ge 1 ; then
-        MONITORDB=$1; shift
-fi
-
-PROXYCACHE=no
-if test $# -ge 1 ; then
-        PROXYCACHE=$1; shift
-fi
-
 SYNCREPL=no
 if test $# -ge 1 ; then
 	SYNCREPL=$1; shift
