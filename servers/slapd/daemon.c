4c5c632d70291c79b7955ae55ba0896776084ffb
Howard Chu <hyc@openldap.org>
2003-10-15 12:16:35 +0000

    ITS#2769 - NT SIGBREAK handling

--- servers/slapd/daemon.c
+++ servers/slapd/daemon.c
@@ -2077,18 +2077,10 @@ slap_sig_shutdown( int sig )
 	 * SIGBREAK is generated when a user logs out.
 	 */
 
-#if 0
 #if HAVE_NT_SERVICE_MANAGER && SIGBREAK
 	if (is_NT_Service && sig == SIGBREAK)
-#ifdef NEW_LOGGING
-	    LDAP_LOG( CONNECTION, CRIT,
-		    "slap_sig_shutdown: SIGBREAK ignored.\n", 0, 0, 0 );
-#else
-	    Debug(LDAP_DEBUG_TRACE, "slap_sig_shutdown: SIGBREAK ignored.\n",
-		  0, 0, 0);
-#endif
+		;
 	else
-#endif
 #endif
 #ifdef SIGHUP
 	if (sig == SIGHUP && global_gentlehup && slapd_gentle_shutdown == 0)
