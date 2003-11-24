80f0a6271016959fd537f23f7f428f6f61367aa3
Kurt Zeilenga <kurt@openldap.org>
2003-11-24 19:23:13 +0000

    This file serves as a template for new files.

---
+++ doc/devel/template.c
@@ -0,0 +1,26 @@
+/* template.c -- example OpenLDAP source file */
+/* $OpenLDAP$ */
+/*
+ * This work is part of OpenLDAP Software.  See main COPYRIGHT file
+ * for detailed notice covering all files in the distribution.
+ */
+ * Copyright YEAR The OpenLDAP Foundation
+ * Portions Copyright YEAR Secondary Rights Holder 
+ * Portions Copyright YEAR Another Rights Holder
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the OpenLDAP
+ * Public License.  A copy of this license is available at
+ * http://www.OpenLDAP.org/license.html or in file LICENSE in the
+ * top-level directory of the distribution.
+ */
+/* Additional (custom) notices (where necessary).
+ *  Please consult Kurt Zeilenga <kurt@openldap.org> before adding
+ *  additional notices.
+ */
+/*
+ * This work was initially developed by John Doe (of FOO Corporation).
+ * This work was (SIGNIFICANTLY) modify by:
+ *   Jane Doe of BAR, Inc. (added this and that)
+ */
