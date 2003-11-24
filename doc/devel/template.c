9203182e749fed2b876e733f0672a08495f8b1a6
Kurt Zeilenga <kurt@openldap.org>
2003-11-24 19:37:36 +0000

    Minor tweak

--- doc/devel/template.c
+++ doc/devel/template.c
@@ -1,9 +1,9 @@
 /* template.c -- example OpenLDAP source file */
 /* $OpenLDAP$ */
-/*
- * This work is part of OpenLDAP Software.  See main COPYRIGHT file
+/* This work is part of OpenLDAP Software.  See main COPYRIGHT file
  * for detailed notice covering all files in the distribution.
  */
+/*
  * Copyright YEAR The OpenLDAP Foundation
  * Portions Copyright YEAR Secondary Rights Holder 
  * Portions Copyright YEAR Another Rights Holder
@@ -11,9 +11,11 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted only as authorized by the OpenLDAP
- * Public License.  A copy of this license is available at
- * http://www.OpenLDAP.org/license.html or in file LICENSE in the
- * top-level directory of the distribution.
+ * Public License.
+ *
+ * A copy of this license is available in the file LICENSE in the
+ * top-level directory of the distribution or, alternatively, at
+ * <http://www.OpenLDAP.org/license.html>.
  */
 /* Additional (custom) notices (where necessary).
  *  Please consult Kurt Zeilenga <kurt@openldap.org> before adding
