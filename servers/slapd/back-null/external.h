0f447a75c415dbf4eabe5e54cf827b89d2741690
Kurt Zeilenga <kurt@openldap.org>
2003-11-26 17:34:58 +0000

    notices and acknowledgements

--- servers/slapd/back-null/external.h
+++ servers/slapd/back-null/external.h
@@ -1,7 +1,22 @@
-/*
- * Copyright 2002-2003 The OpenLDAP Foundation, All Rights Reserved.
- * COPYING RESTRICTIONS APPLY, see COPYRIGHT file
+/* $OpenLDAP$ */
+/* This work is part of OpenLDAP Software <http://www.openldap.org/>.
+ *
+ * Copyright 2002-2003 The OpenLDAP Foundation.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the OpenLDAP
+ * Public License.
+ *
+ * A copy of this license is available in the file LICENSE in the
+ * top-level directory of the distribution or, alternatively, at
+ * <http://www.OpenLDAP.org/license.html>.
  */
+/* ACKNOWLEDGEMENTS:
+ * This work was originally developed by Howard Chu for inclusion
+ * in OpenLDAP Software.
+ */
+
 #ifndef _NULL_EXTERNAL_H
 #define _NULL_EXTERNAL_H
 
