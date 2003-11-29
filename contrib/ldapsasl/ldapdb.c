2a2fc44cdeb448e6e3a3aa76d669389e5c090144
Howard Chu <hyc@openldap.org>
2003-11-29 23:04:39 +0000

    Use OpenLDAP Public License

--- contrib/ldapsasl/ldapdb.c
+++ contrib/ldapsasl/ldapdb.c
@@ -1,22 +1,14 @@
+/* $OpenLDAP$ */
 /* SASL LDAP auxprop implementation
  * Copyright (C) 2002,2003 Howard Chu, All rights reserved. <hyc@symas.com>
  *
- * Permission is granted to anyone to use this software for any purpose
- * on any computer system, and to alter it and redistribute it, subject
- * to the following restrictions:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the OpenLDAP
+ * Public License.
  *
- * 1. The author is not responsible for the consequences of use of this
- *    software, no matter how awful, even if they arise from flaws in it.
- *
- * 2. The origin of this software must not be misrepresented, either by
- *    explicit claim or by omission.  Since few users ever read sources,
- *    credits should appear in the documentation.
- *
- * 3. Altered versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.  Since few users
- *    ever read sources, credits should appear in the documentation.
- *
- * 4. This notice may not be removed or altered.
+ * A copy of this license is available in the file LICENSE in the
+ * top-level directory of the distribution or, alternatively, at
+ * <http://www.OpenLDAP.org/license.html>.
  */
 
 #include <config.h>
