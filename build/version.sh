6ef731db512e6df8fafdb8996ee6643c5a9add9d
Kurt Zeilenga <kurt@openldap.org>
2004-03-24 22:42:16 +0000

    Update library soname handling

--- build/version.sh
+++ build/version.sh
@@ -18,14 +18,16 @@ DIR=`dirname $0`
 
 if test $ol_patch != X ; then
 	ol_version=${ol_major}.${ol_minor}.${ol_patch}
+	ol_api_lib_release=${ol_major}.${ol_minor}
 	ol_type=Release
 elif test $ol_minor != X ; then
 	ol_version=${ol_major}.${ol_minor}.${ol_patch}
+	ol_api_lib_release=${ol_major}.${ol_minor}-releng
 	ol_type=Engineering
 else
 	ol_version=${ol_major}.${ol_minor}
+	ol_api_lib_release=${ol_major}-devel
 	ol_type=Devel
-	ol_api_lib=0
 fi
 
 ol_string="${ol_package} ${ol_version}-${ol_type}"
@@ -35,7 +37,8 @@ echo OL_MAJOR=$ol_major
 echo OL_MINOR=$ol_minor
 echo OL_PATCH=$ol_patch
 echo OL_API_INC=$ol_api_inc
-echo OL_API_LIB=$ol_api_lib
+echo OL_API_LIB_RELEASE=$ol_api_lib_release
+echo OL_API_LIB_VERSION=$ol_api_lib_version
 echo OL_VERSION=$ol_version
 echo OL_TYPE=$ol_type
 echo OL_STRING=\"${ol_string}\"
