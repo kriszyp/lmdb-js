158e5727b4f330e1c17c242153eb8a821295286b
Kurt Zeilenga <kurt@openldap.org>
2004-04-06 19:32:17 +0000

    ITS#3056 partial fix - from a slurpd perspective, the updatedn should be allowed to the entry at the naming prefix (database suffix).

--- servers/slapd/back-bdb/add.c
+++ servers/slapd/back-bdb/add.c
@@ -298,7 +298,7 @@ retry:	/* transaction retry */
 		 * no parent!
 		 *  if not attempting to add entry at suffix or with parent ""
 		 */
-		if (( !be_isroot( op ) || pdn.bv_len > 0 )
+		if ((( !be_isroot( op ) && !be_isupdate(op)) || pdn.bv_len > 0 )
 			&& !is_entry_glue( op->oq_add.rs_e ))
 		{
 #ifdef NEW_LOGGING
