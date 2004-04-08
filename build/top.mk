1b4a03c3db8877a7a5ee7c2c4ab9cd804bce4857
Howard Chu <hyc@openldap.org>
2004-04-08 06:01:07 +0000

    More static/dynamic cleanup

--- build/top.mk
+++ build/top.mk
@@ -77,7 +77,9 @@ LTONLY_LIB = $(@PLAT@_LTONLY_LIB)
 
 # libtool --only flag for modules: depends on linkage of module
 # The BUILD_MOD_DYNAMIC macro is defined in each backend Makefile.in file
-LTONLY_MOD = # --only-$(BUILD_MOD_DYNAMIC)
+LTONLY_yes = static
+LTONLY_mod = shared
+LTONLY_MOD = # --only-$(BUILD_MOD)
 
 # platform-specific libtool flags
 NT_LTFLAGS_LIB = -no-undefined -avoid-version -rpath $(libdir)
