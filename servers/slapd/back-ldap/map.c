176e5542bb2a3c710ee5174425d41190dc2545c5
Pierangelo Masarati <ando@openldap.org>
2004-06-20 22:42:04 +0000

    fix erroneous logging

--- servers/slapd/back-ldap/map.c
+++ servers/slapd/back-ldap/map.c
@@ -495,11 +495,11 @@ ldap_back_filter_map_rewrite(
 #ifdef NEW_LOGGING
 		LDAP_LOG( BACK_LDAP, DETAIL1, 
 			"[rw] %s: \"%s\" -> \"%s\"\n",
-			dc->ctx, ftmp.bv_val, fstr->bv_val );		
+			fdc.ctx, ftmp.bv_val, fstr->bv_val );		
 #else /* !NEW_LOGGING */
 		Debug( LDAP_DEBUG_ARGS,
 			"[rw] %s: \"%s\" -> \"%s\"\n",
-			dc->ctx, ftmp.bv_val, fstr->bv_val );		
+			fdc.ctx, ftmp.bv_val, fstr->bv_val );		
 #endif /* !NEW_LOGGING */
 		rc = LDAP_SUCCESS;
 		break;
