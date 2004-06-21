52e8543b7b50c4f41b8a10973bf051da1724f4c3
Pierangelo Masarati <ando@openldap.org>
2004-06-21 16:46:41 +0000

    parse page control only in case of succes...

--- clients/tools/ldapsearch.c
+++ clients/tools/ldapsearch.c
@@ -1044,7 +1044,7 @@ static int dosearch(
 			case LDAP_RES_SEARCH_RESULT:
 				rc = print_result( ld, msg, 1 );
 #ifdef LDAP_CONTROL_PAGEDRESULTS
-				if ( pageSize != 0 ) { 
+				if ( rc == LDAP_SUCCESS && pageSize != 0 ) { 
 					rc = parse_page_control( ld, msg, &cookie );
 				}
 #endif
