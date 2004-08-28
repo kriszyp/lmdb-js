f39d6f80fba5dce731f1146643657bb952b9de7a
Kurt Zeilenga <kurt@openldap.org>
2004-08-28 00:50:15 +0000

    ITS#3275: fix logging

--- servers/slurpd/replog.c
+++ servers/slurpd/replog.c
@@ -139,13 +139,13 @@ copy_replog(
     dfp = lock_fopen( dst, "a", &dlfp );
     if ( dfp == NULL ) {
 #ifdef NEW_LOGGING
-	LDAP_LOG ( SLURPD, ERR, "copy_replog: "
-		"Error: Can't lock replog \"%s\" for write: %s\n",
-		src, sys_errlist[ errno ], 0 );
+		LDAP_LOG ( SLURPD, ERR,
+			"copy_replog: Error: Can't lock replog \"%s\" for write: %s\n",
+			dst, sys_errlist[ errno ], 0 );
 #else
-	Debug( LDAP_DEBUG_ANY,
-		"Error: copy_replog: Can't lock replog \"%s\" for write: %s\n",
-		src, sys_errlist[ errno ], 0 );
+		Debug( LDAP_DEBUG_ANY,
+			"Error: copy_replog: Can't lock replog \"%s\" for write: %s\n",
+			dst, sys_errlist[ errno ], 0 );
 #endif
 	lock_fclose( rfp, lfp );
 	return( 1 );
@@ -165,12 +165,13 @@ copy_replog(
 
     if ( lock_fclose( dfp, dlfp ) == EOF ) {
 #ifdef NEW_LOGGING
-	LDAP_LOG ( SLURPD, ERR, "copy_replog: "
-		"Error: Error closing \"%s\"\n", src, 0, 0 );
+		LDAP_LOG ( SLURPD, ERR,
+			"copy_replog: Error: Error closing \"%s\"\n",
+			dst, 0, 0 );
 #else
-	Debug( LDAP_DEBUG_ANY,
-		"Error: copy_replog: Error closing \"%s\"\n",
-		src, 0, 0 );
+		Debug( LDAP_DEBUG_ANY,
+			"Error: copy_replog: Error closing \"%s\"\n",
+			dst, 0, 0 );
 #endif
     }
     if ( lock_fclose( rfp, lfp ) == EOF ) {
