768f7af96b77e6931bd496e552986fc35167998a
Howard Chu <hyc@openldap.org>
2004-09-28 05:05:16 +0000

    Revert 1.452.2.27 to sync with rest of tree

--- servers/slapd/slap.h
+++ servers/slapd/slap.h
@@ -1632,10 +1632,6 @@ typedef struct req_add_s {
 	Entry *rs_e;
 } req_add_s;
 
-typedef struct req_delete_s {
-	struct berval rd_csn;
-} req_delete_s;
-
 typedef struct req_abandon_s {
 	ber_int_t rs_msgid;
 } req_abandon_s;
@@ -1998,7 +1994,6 @@ typedef struct slap_op {
 		req_add_s oq_add;
 		req_bind_s oq_bind;
 		req_compare_s oq_compare;
-		req_delete_s oq_delete;
 		req_modify_s oq_modify;
 		req_modrdn_s oq_modrdn;
 		req_search_s oq_search;
@@ -2012,7 +2007,6 @@ typedef struct slap_op {
 #define oq_add o_request.oq_add
 #define oq_bind o_request.oq_bind
 #define oq_compare o_request.oq_compare
-#define oq_delete o_request.oq_delete
 #define oq_modify o_request.oq_modify
 #define oq_modrdn o_request.oq_modrdn
 #define oq_search o_request.oq_search
@@ -2026,8 +2020,6 @@ typedef struct slap_op {
 #define orb_cred oq_bind.rb_cred
 #define orb_edn oq_bind.rb_edn
 #define orb_ssf oq_bind.rb_ssf
-
-#define ord_csn oq_delete.rd_csn
 
 #define ors_scope oq_search.rs_scope
 #define ors_deref oq_search.rs_deref
