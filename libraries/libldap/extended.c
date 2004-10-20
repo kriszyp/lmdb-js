b32d83ad6632a981f182c9bea206ed509c0aac99
Kurt Zeilenga <kurt@openldap.org>
2004-10-20 22:06:50 +0000

    Sync with HEAD Fix LDAP IMR tags

--- libraries/libldap/extended.c
+++ libraries/libldap/extended.c
@@ -353,7 +353,13 @@ ldap_parse_intermediate (
 
 	tag = ber_peek_tag( ber, &len );
 
-	if( tag == LDAP_TAG_EXOP_RES_OID ) {
+	/*
+	 * NOTE: accept intermediate and extended response tag values
+	 * as older versions of slapd(8) incorrectly used extended
+	 * response tags.
+	 * Should be removed when 2.2 is moved to Historic.
+	 */
+	if( tag == LDAP_TAG_IM_RES_OID || tag == LDAP_TAG_EXOP_RES_OID ) {
 		/* we have a resoid */
 		if( ber_scanf( ber, "a", &resoid ) == LBER_ERROR ) {
 			ld->ld_errno = LDAP_DECODING_ERROR;
@@ -364,7 +370,7 @@ ldap_parse_intermediate (
 		tag = ber_peek_tag( ber, &len );
 	}
 
-	if( tag == LDAP_TAG_EXOP_RES_VALUE ) {
+	if( tag == LDAP_TAG_IM_RES_VALUE || tag == LDAP_TAG_EXOP_RES_VALUE ) {
 		/* we have a resdata */
 		if( ber_scanf( ber, "O", &resdata ) == LBER_ERROR ) {
 			ld->ld_errno = LDAP_DECODING_ERROR;
