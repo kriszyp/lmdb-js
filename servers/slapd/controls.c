38b430be3cbd83c49978e0f3d5320c57645b78cd
Howard Chu <hyc@openldap.org>
2004-11-27 11:50:54 +0000

    Fix prev commit

--- servers/slapd/controls.c
+++ servers/slapd/controls.c
@@ -354,8 +354,8 @@ slap_find_control_id(
 	int *cid )
 {
 	slap_control *ctrl = find_ctrl( oid );
-	if ( ctrl && cid ) {
-		*cid = ctrl->sc_cid;
+	if ( ctrl ) {
+		if ( cid ) *cid = ctrl->sc_cid;
 		return LDAP_SUCCESS;
 	}
 	return LDAP_CONTROL_NOT_FOUND;
