0145d54325674942cc1752d0e82d20e237da4f2f
Howard Chu <hyc@openldap.org>
2004-11-29 04:03:56 +0000

    In build_new_dn, rdn may be part of a larger string, use strncopy.

--- servers/slapd/dn.c
+++ servers/slapd/dn.c
@@ -1299,7 +1299,7 @@ build_new_dn( struct berval * new_dn,
 	new_dn->bv_len = parent_dn->bv_len + newrdn->bv_len + 1;
 	new_dn->bv_val = (char *) slap_sl_malloc( new_dn->bv_len + 1, memctx );
 
-	ptr = lutil_strcopy( new_dn->bv_val, newrdn->bv_val );
+	ptr = lutil_strncopy( new_dn->bv_val, newrdn->bv_val, newrdn->bv_len );
 	*ptr++ = ',';
 	strcpy( ptr, parent_dn->bv_val );
 }
