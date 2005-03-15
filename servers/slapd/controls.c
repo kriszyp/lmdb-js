a0b32e7ab07727a37e821a461906a000774f3797
Kurt Zeilenga <kurt@openldap.org>
2005-03-15 02:12:51 +0000

    ITS#3563 fix parseSubentries bug

--- servers/slapd/controls.c
+++ servers/slapd/controls.c
@@ -1207,8 +1207,8 @@ static int parseSubentries (
 
 	/* FIXME: should use BER library */
 	if( ( ctrl->ldctl_value.bv_len != 3 )
-		&& ( ctrl->ldctl_value.bv_val[0] != 0x01 )
-		&& ( ctrl->ldctl_value.bv_val[1] != 0x01 ))
+		|| ( ctrl->ldctl_value.bv_val[0] != 0x01 )
+		|| ( ctrl->ldctl_value.bv_val[1] != 0x01 ))
 	{
 		rs->sr_text = "subentries control value encoding is bogus";
 		return LDAP_PROTOCOL_ERROR;
