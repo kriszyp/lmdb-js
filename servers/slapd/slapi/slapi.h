35942c40ae008b7c396a652f17b80429681846fc
Luke Howard <lukeh@openldap.org>
2005-07-23 13:14:13 +0000

    update for new backend types

--- servers/slapd/slapi/slapi.h
+++ servers/slapd/slapi/slapi.h
@@ -120,7 +120,7 @@ struct slapi_pblock {
  * Was: plugin.h
  */
 
-#define SLAPI_PLUGIN_IS_POST_FN(x) ((x) >= SLAPI_PLUGIN_POST_BIND_FN && (x) <= SLAPI_PLUGIN_POST_RESULT_FN)
+#define SLAPI_PLUGIN_IS_POST_FN(x) ((x) >= SLAPI_PLUGIN_POST_BIND_FN && (x) <= SLAPI_PLUGIN_BE_POST_DELETE_FN)
 
 /* really private stuff */
 #define SLAPI_X_CONFIG_ARGV			1400
