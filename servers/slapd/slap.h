a79ae8da3e866a575b3da55d3b23536610a708df
Howard Chu <hyc@openldap.org>
2005-10-06 20:34:30 +0000

    Better fix for req_pwdexop_s

--- servers/slapd/slap.h
+++ servers/slapd/slap.h
@@ -1948,9 +1948,7 @@ typedef struct req_extended_s {
 } req_extended_s;
 
 typedef struct req_pwdexop_s {
-	struct berval rs_reqoid;
-	int rs_flags;
-	struct berval *rs_reqdata;
+	struct req_extended_s rs_extended;
 	struct berval rs_old;
 	struct berval rs_new;
 	Modifications *rs_mods;
