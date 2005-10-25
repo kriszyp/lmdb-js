2f9965c32b9980c26f1fb2ae8f491083138b046b
Howard Chu <hyc@openldap.org>
2005-10-25 10:54:04 +0000

    Lower id2entry cache priority in tool mode

--- servers/slapd/back-bdb/init.c
+++ servers/slapd/back-bdb/init.c
@@ -426,6 +426,10 @@ bdb_db_open( BackendDB *be )
 		}
 
 		if( i == BDB_ID2ENTRY ) {
+			if ( slapMode & SLAP_TOOL_MODE )
+				db->bdi_db->mpf->set_priority( db->bdi_db->mpf,
+					DB_PRIORITY_VERY_LOW );
+
 			rc = db->bdi_db->set_pagesize( db->bdi_db,
 				BDB_ID2ENTRY_PAGESIZE );
 			if ( slapMode & SLAP_TOOL_READMAIN ) {
@@ -481,13 +485,6 @@ bdb_db_open( BackendDB *be )
 				buf, db_strerror(rc), rc );
 			return rc;
 		}
-
-#if 0
-		if( i == BDB_ID2ENTRY && ( slapMode & SLAP_TOOL_MODE )) {
-			db->bdi_db->mpf->set_priority( db->bdi_db->mpf,
-				DB_PRIORITY_VERY_LOW );
-		}
-#endif
 
 		flags &= ~(DB_CREATE | DB_RDONLY);
 		db->bdi_name = bdbi_databases[i].name;
