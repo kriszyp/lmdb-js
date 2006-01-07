41a341abf66373912e913bd61086feda98dd8667
Kurt Zeilenga <kurt@openldap.org>
2006-01-07 19:51:55 +0000

    remove lint

--- servers/slapd/back-bdb/cache.c
+++ servers/slapd/back-bdb/cache.c
@@ -618,8 +618,10 @@ bdb_cache_lru_purge(void *ctx, void *arg)
 		/* Defer processing till we're needed again */
 		ldap_pvt_runqueue_resched( &slapd_rq, rtask, 1 );
 		ldap_pvt_thread_mutex_unlock( &slapd_rq.rq_mutex );
-		}
 	}
+
+	return NULL;
+}
 
 /* caller must have lru_head_mutex locked. mutex
  * will be unlocked on return.
