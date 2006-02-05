f96e6378d6cd06c744a47af5e5e551cbb494826a
Howard Chu <hyc@openldap.org>
2006-02-05 01:05:38 +0000

    filter2bv can't de-normalize UUIDs, must do it explicitly for back-ldap in test045

--- servers/slapd/syncrepl.c
+++ servers/slapd/syncrepl.c
@@ -2051,8 +2051,12 @@ syncrepl_del_nonpresent(
 
 		for (i=0; uuids[i].bv_val; i++) {
 			op->ors_slimit = 1;
+			slap_uuidstr_from_normalized( &uf.f_av_value, &uuids[i],
+				op->o_tmpmemctx );
+			filter2bv_x( op, op->ors_filter, &op->ors_filterstr );
 			uf.f_av_value = uuids[i];
 			rc = be->be_search( op, &rs_search );
+			op->o_tmpfree( op->ors_filterstr.bv_val, op->o_tmpmemctx );
 		}
 		si->si_refreshDelete ^= NP_DELETE_ONE;
 	} else {
