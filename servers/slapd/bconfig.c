328470c1148c16027b6f5c67cf0acaa490d29c34
Howard Chu <hyc@openldap.org>
2006-04-27 02:13:21 +0000

    OIDs for more overlays

--- servers/slapd/bconfig.c
+++ servers/slapd/bconfig.c
@@ -213,6 +213,11 @@ static OidRec OidMacros[] = {
  * OLcfgOv{Oc|At}:7			-> distproc
  * OLcfgOv{Oc|At}:8			-> dynlist
  * OLcfgOv{Oc|At}:9			-> dds
+ * OLcfgOv{Oc|At}:10		-> unique
+ * OLcfgOv{Oc|At}:11		-> refint
+ * OLcfgOv{Oc|At}:12 		-> ppolicy
+ * OLcfgOv{Oc|At}:13		-> constraint
+ * OLcfgOv{Oc|At}:14		-> translucent
  */
 
 /* alphabetical ordering */
