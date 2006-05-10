b371199b68eef73ff69dc212cb874f9baadc9679
Kurt Zeilenga <kurt@openldap.org>
2006-05-10 16:53:20 +0000

    ITS#4532 fix

--- servers/slapd/result.c
+++ servers/slapd/result.c
@@ -1476,6 +1476,7 @@ int slap_read_controls(
 	myop.o_bd = NULL;
 	myop.o_res_ber = ber;
 	myop.o_callback = NULL;
+	myop.ors_slimit = 1;
 
 	rc = slap_send_search_entry( &myop, rs );
 	if( rc ) return rc;
