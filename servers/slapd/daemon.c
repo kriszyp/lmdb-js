d7be822a2a501b4045feae488ae464f64317d9b4
Howard Chu <hyc@openldap.org>
2006-08-19 06:27:11 +0000

    Fix prev commit

--- servers/slapd/daemon.c
+++ servers/slapd/daemon.c
@@ -528,7 +528,8 @@ void slapd_remove(
 
 	SLAP_DEL_SOCK(s);
 
-	ber_sockbuf_free(sb);
+	if ( sb )
+		ber_sockbuf_free(sb);
 
 	/* If we ran out of file descriptors, we dropped a listener from
 	 * the select() loop. Now that we're removing a session from our
@@ -1265,7 +1266,7 @@ close_listeners(
 		Listener *lr = slap_listeners[l];
 
 		if ( lr->sl_sd != AC_SOCKET_INVALID ) {
-			if ( remove ) slapd_remove( lr->sl_sd, 0, 0, 0 );
+			if ( remove ) slapd_remove( lr->sl_sd, NULL, 0, 0, 0 );
 
 #ifdef LDAP_PF_LOCAL
 			if ( lr->sl_sa.sa_addr.sa_family == AF_LOCAL ) {
