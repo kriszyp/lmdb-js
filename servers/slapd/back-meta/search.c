b1a2be99c6f9391245f620a3871ecb7b39569918
Pierangelo Masarati <ando@openldap.org>
2006-10-14 09:26:22 +0000

    don't mix return codes (ITS#4711)

--- servers/slapd/back-meta/search.c
+++ servers/slapd/back-meta/search.c
@@ -392,17 +392,17 @@ meta_back_search_start(
 	dc->target = mt;
 	dc->ctx = "searchBase";
 	switch ( ldap_back_dn_massage( dc, &realbase, &mbase ) ) {
-	default:
+	case LDAP_SUCCESS:
 		break;
 
-	case REWRITE_REGEXEC_UNWILLING:
+	case LDAP_UNWILLING_TO_PERFORM:
 		rs->sr_err = LDAP_UNWILLING_TO_PERFORM;
 		rs->sr_text = "Operation not allowed";
 		send_ldap_result( op, rs );
 		retcode = META_SEARCH_ERR;
 		goto doreturn;
 
-	case REWRITE_REGEXEC_ERR:
+	default:
 
 		/*
 		 * this target is no longer candidate
