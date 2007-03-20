23ba7dfa121c12baa6f1133b02903d24e1667a60
Howard Chu <hyc@openldap.org>
2007-03-20 12:29:06 +0000

    Add ber_skip_data() to help positioning in structured BER data

--- libraries/liblber/io.c
+++ libraries/liblber/io.c
@@ -48,6 +48,25 @@
 #include "ldap_log.h"
 
 ber_slen_t
+ber_skip_data(
+	BerElement *ber,
+	ber_len_t len )
+{
+	ber_len_t	actuallen, nleft;
+
+	assert( ber != NULL );
+
+	assert( LBER_VALID( ber ) );
+
+	nleft = ber_pvt_ber_remaining( ber );
+	actuallen = nleft < len ? nleft : len;
+	ber->ber_ptr += actuallen;
+	ber->ber_tag = *(unsigned char *)ber->ber_ptr;
+
+	return( (ber_slen_t) actuallen );
+}
+
+ber_slen_t
 ber_read(
 	BerElement *ber,
 	char *buf,
