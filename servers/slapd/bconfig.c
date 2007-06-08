f3fccb7783885f4ce218b9035fee47e532941186
Howard Chu <hyc@openldap.org>
2007-06-08 10:07:34 +0000

    ITS#4949 config_syslog fix from HEAD

--- servers/slapd/bconfig.c
+++ servers/slapd/bconfig.c
@@ -2216,8 +2216,6 @@ config_loglevel(ConfigArgs *c) {
 		return 0;
 	}
 
-	config_syslog = 0;
-
 	for( i=1; i < c->argc; i++ ) {
 		int	level;
 
@@ -2236,7 +2234,11 @@ config_loglevel(ConfigArgs *c) {
 				return( 1 );
 			}
 		}
-		config_syslog |= level;
+		/* Explicitly setting a zero clears all the levels */
+		if ( level )
+			config_syslog |= level;
+		else
+			config_syslog = 0;
 	}
 	if ( slapMode & SLAP_SERVER_MODE ) {
 		ldap_syslog = config_syslog;
