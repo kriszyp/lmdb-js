b9f012377a7bbaa6c657f89a64323de327a53e45
Howard Chu <hyc@openldap.org>
2007-10-23 21:14:39 +0000

    ITS#5197 fully revert prev commit, use thr_stub

--- servers/slapd/result.c
+++ servers/slapd/result.c
@@ -743,14 +743,12 @@ slap_send_search_entry( Operation *op, SlapReply *rs )
 		return LDAP_SIZELIMIT_EXCEEDED;
 	}
 
-#ifndef NO_THREADS
 	/* Every 64 entries, check for thread pool pause */
 	if (( rs->sr_nentries & 0x3f == 0x3f ) &&
 		ldap_pvt_thread_pool_query( &connection_pool,
 			LDAP_PVT_THREAD_POOL_PARAM_PAUSING, &i ) == 0 && i ) {
 		return LDAP_BUSY;
 	}
-#endif
 
 	rs->sr_type = REP_SEARCH;
 
