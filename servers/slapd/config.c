eec64ca58f53f97bd0379ab004a98f24a8af788d
Howard Chu <hyc@openldap.org>
2007-12-14 04:15:38 +0000

    ITS#5276 from HEAD

--- servers/slapd/config.c
+++ servers/slapd/config.c
@@ -843,6 +843,10 @@ read_config_file(const char *fname, int depth, ConfigArgs *cf, ConfigTable *cft)
 	rc = 0;
 
 done:
+	if ( cf ) {
+		cf->be = c->be;
+		cf->bi = c->bi;
+	}
 	ch_free(c->tline);
 	fclose(fp);
 	ch_free(c->argv);
