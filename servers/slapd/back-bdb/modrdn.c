82719d535119ed8875e6f60b9e5383583c13480e
Howard Chu <hyc@openldap.org>
2008-01-11 04:01:37 +0000

    ITS#5319 no special treatment for no-effect renames

--- servers/slapd/back-bdb/modrdn.c
+++ servers/slapd/back-bdb/modrdn.c
@@ -591,27 +591,6 @@ retry:	/* transaction retry */
 		}                   
 	}
 
-	/* Rename to exactly the same name, noop */
-	if ( nei == ei && bvmatch( &e->e_name, &new_dn )) {
-		rs->sr_err = LDAP_SUCCESS;
-		if ( op->o_postread ) {
-			if( postread_ctrl == NULL ) {
-				postread_ctrl = &ctrls[num_ctrls++];
-				ctrls[num_ctrls] = NULL;
-			}
-			if( slap_read_controls( op, rs, e,
-				&slap_post_read_bv, postread_ctrl ) )
-			{
-				Debug( LDAP_DEBUG_TRACE,        
-					"<=- " LDAP_XSTRING(bdb_modrdn)
-					": post-read failed!\n", 0, 0, 0 );
-			}                   
-		}
-		TXN_ABORT( ltid );
-		ltid = NULL;
-		goto return_results;
-	}
-
 	/* nested transaction */
 	rs->sr_err = TXN_BEGIN( bdb->bi_dbenv, ltid, &lt2, bdb->bi_db_opflags );
 	rs->sr_text = NULL;
