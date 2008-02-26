b3e5e0459599ec6dd0f7453bd6bade52ee9a4b15
Ralf Haferkamp <ralf@openldap.org>
2008-02-26 13:34:59 +0000

    Remove request from active requests stack in exception handler

--- contrib/ldapc++/src/LDAPMessageQueue.cpp
+++ contrib/ldapc++/src/LDAPMessageQueue.cpp
@@ -50,7 +50,7 @@ LDAPMsg *LDAPMessageQueue::getNext(){
         ret = req->getNextMessage();
     }catch(LDAPException e){
         //do some clean up
-        m_activeReq.top();
+        m_activeReq.pop();
         throw;   
     }
 
