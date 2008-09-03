f7e3566ceb66c65a5f581f073ceaead4d6b9e860
Quanah Gibson-Mount <quanah@openldap.org>
2008-09-03 22:58:00 +0000

    Add back-ndb

--- tests/scripts/acfilter.sh
+++ tests/scripts/acfilter.sh
@@ -13,6 +13,10 @@
 ## top-level directory of the distribution or, alternatively, at
 ## <http://www.OpenLDAP.org/license.html>.
 #
-# Strip comments
+# Strip comments, sort attributes. Requires GNU awk
 #
+if [ "$BACKEND" != ndb ]; then
 grep -v '^#'
+else
+grep -v '^#'| awk 'BEGIN{FS="\n";RS=""} {j=0; for (i=1; i<=NF; i++){ if ($i ~ /^ /){ x[j] = x[j] "\n" $i; } else { j++; x[j] = $i } } print x[1]; delete x[1]; j=asort(x); for (i=1; i<=j; i++){ print x[i]; } delete x; print "" }'
+fi
