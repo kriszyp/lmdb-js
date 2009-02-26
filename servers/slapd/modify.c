8e5ebf4dfbbedbda7485ddf0c8a62995f273c934
Howard Chu <hyc@openldap.org>
2009-02-26 23:16:41 +0000

    Fix prev commit

--- servers/slapd/modify.c
+++ servers/slapd/modify.c
@@ -808,7 +808,7 @@ slap_sort_vals(
 		}
 	}
 	done:
-	if ( rc == LDAP_TYPE_OR_VALUE_EXISTS && i >= 0 )
+	if ( match == 0 && i >= 0 )
 		*dup = ix[i];
 
 	/* For sorted attributes, put the values in index order */
