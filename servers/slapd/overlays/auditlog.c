b078edc82c69d6da347da3f2844791a50c2875b0
Quanah Gibson-Mount <quanah@openldap.org>
2009-09-29 23:43:53 +0000

    ITS#6286

--- servers/slapd/overlays/auditlog.c
+++ servers/slapd/overlays/auditlog.c
@@ -73,7 +73,7 @@ static int auditlog_response(Operation *op, SlapReply *rs) {
 	Attribute *a;
 	Modifications *m;
 	struct berval *b, *who = NULL;
-	char *what, *suffix;
+	char *what, *whatm, *suffix;
 	time_t stamp;
 	int i;
 
@@ -147,15 +147,15 @@ static int auditlog_response(Operation *op, SlapReply *rs) {
 	  case LDAP_REQ_MODIFY:
 		for(m = op->orm_modlist; m; m = m->sml_next) {
 			switch(m->sml_op & LDAP_MOD_OP) {
-				case LDAP_MOD_ADD:	 what = "add";		break;
-				case LDAP_MOD_REPLACE:	 what = "replace";	break;
-				case LDAP_MOD_DELETE:	 what = "delete";	break;
-				case LDAP_MOD_INCREMENT: what = "increment";	break;
+				case LDAP_MOD_ADD:	 whatm = "add";		break;
+				case LDAP_MOD_REPLACE:	 whatm = "replace";	break;
+				case LDAP_MOD_DELETE:	 whatm = "delete";	break;
+				case LDAP_MOD_INCREMENT: whatm = "increment";	break;
 				default:
 					fprintf(f, "# MOD_TYPE_UNKNOWN:%02x\n", m->sml_op & LDAP_MOD_OP);
 					continue;
 			}
-			fprintf(f, "%s: %s\n", what, m->sml_desc->ad_cname.bv_val);
+			fprintf(f, "%s: %s\n", whatm, m->sml_desc->ad_cname.bv_val);
 			if((b = m->sml_values) != NULL)
 			  for(i = 0; b[i].bv_val; i++)
 				fprint_ldif(f, m->sml_desc->ad_cname.bv_val, b[i].bv_val, b[i].bv_len);
