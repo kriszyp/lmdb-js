11e2ac526c1b93be870872feac68f5d0439e3eeb
Hallvard Furuseth <hallvard@openldap.org>
2011-02-02 22:48:12 +0000

    Fix last ldapwhoami commit.
    Ensure message is initialized before freeing it.

--- clients/tools/ldapwhoami.c
+++ clients/tools/ldapwhoami.c
@@ -113,7 +113,7 @@ main( int argc, char *argv[] )
 	char		*matcheddn = NULL, *text = NULL, **refs = NULL;
 	struct berval	*authzid = NULL;
 	int		id, code = 0;
-	LDAPMessage	*res;
+	LDAPMessage	*res = NULL;
 	LDAPControl	**ctrls = NULL;
 
 	tool_init( TOOL_WHOAMI );
