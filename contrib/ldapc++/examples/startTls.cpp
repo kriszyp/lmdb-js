cd1f718f014e09a315cb2f83d0581baccdbab64c
Kurt Zeilenga <kurt@OpenLDAP.org>
2011-03-28 18:08:19 -0700

    Update guide Copyright/License handling Avoids redundant files. And update various notices

--- contrib/ldapc++/examples/startTls.cpp
+++ contrib/ldapc++/examples/startTls.cpp
@@ -1,6 +1,6 @@
 // $OpenLDAP$
 /*
- * Copyright 2010, OpenLDAP Foundation, All Rights Reserved.
+ * Copyright 2010-2011 The OpenLDAP Foundation, All Rights Reserved.
  * COPYING RESTRICTIONS APPLY, see COPYRIGHT file
  */
 
--- contrib/ldapc++/src/LDAPResult.cpp
+++ contrib/ldapc++/src/LDAPResult.cpp
@@ -1,6 +1,6 @@
 // $OpenLDAP$
 /*
- * Copyright 2000-2007, OpenLDAP Foundation, All Rights Reserved.
+ * Copyright 2000-2011 The OpenLDAP Foundation, All Rights Reserved.
  * COPYING RESTRICTIONS APPLY, see COPYRIGHT file
  */
 
