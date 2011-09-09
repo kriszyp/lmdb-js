80f6f0a1c02b3146473b04ff79847b10717cab2e
Howard Chu <hyc@openldap.org>
2011-09-08 22:02:59 -0700

    Drop referral.c

--- servers/slapd/back-mdb/init.c
+++ servers/slapd/back-mdb/init.c
@@ -411,9 +411,7 @@ mdb_back_initialize(
 
 	bi->bi_extended = mdb_extended;
 
-#if 0	/* FIXME: Redundant, why does this exist? */
-	bi->bi_chk_referrals = mdb_referrals;
-#endif
+	bi->bi_chk_referrals = 0;
 	bi->bi_operational = mdb_operational;
 
 	bi->bi_has_subordinates = mdb_hasSubordinates;
