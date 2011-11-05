a6cb0cfece538e564fb97f2ec7dee9298d575531
Quanah Gibson-Mount <quanah@openldap.org>
2011-11-04 17:19:20 -0700

    Missed portion of 04f8fa5619fed389f238d780129d535a0947ee6f

--- servers/slapd/overlays/rwm.c
+++ servers/slapd/overlays/rwm.c
@@ -911,6 +911,7 @@ rwm_entry_get_rw( Operation *op, struct berval *ndn,
 		/* duplicate & release */
 		op2.o_bd->bd_info = (BackendInfo *)on;
 		rc = rwm_send_entry( &op2, &rs );
+		RS_ASSERT( rs.sr_flags & REP_ENTRY_MUSTFLUSH );
 		if ( rc == SLAP_CB_CONTINUE ) {
 			*ep = rs.sr_entry;
 			rc = LDAP_SUCCESS;
@@ -1498,6 +1499,7 @@ rwm_send_entry( Operation *op, SlapReply *rs )
 	} else if ( rs->sr_flags & REP_ENTRY_MUSTRELEASE ) {
 		/* ITS#6423: REP_ENTRY_MUSTRELEASE incompatible
 		 * with REP_ENTRY_MODIFIABLE */
+		RS_ASSERT( 0 );
 		rc = 1;
 		goto fail;
 	}
