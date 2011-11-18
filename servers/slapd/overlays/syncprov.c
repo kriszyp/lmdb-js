7be5837dc097bdea76403b70d0a4a61da27a3764
Howard Chu <hyc@openldap.org>
2011-11-18 01:22:52 -0800

    Cleanup syncprov_db_close/CONFIG_DELETE code
    No mutex needed since config ops are single-threaded.
    Don't send Unavailable message if we're shutting down.

--- servers/slapd/overlays/syncprov.c
+++ servers/slapd/overlays/syncprov.c
@@ -3168,16 +3168,16 @@ syncprov_db_close(
 	}
 
 #ifdef SLAP_CONFIG_DELETE
-	ldap_pvt_thread_mutex_lock( &si->si_ops_mutex );
-	for ( so=si->si_ops, sonext=so;  so; so=sonext  ) {
-		SlapReply rs = {REP_RESULT};
-		rs.sr_err = LDAP_UNAVAILABLE;
-		send_ldap_result( so->s_op, &rs );
-		sonext=so->s_next;
-		syncprov_drop_psearch( so, 0);
+	if ( !slapd_shutdown ) {
+		for ( so=si->si_ops, sonext=so;  so; so=sonext  ) {
+			SlapReply rs = {REP_RESULT};
+			rs.sr_err = LDAP_UNAVAILABLE;
+			send_ldap_result( so->s_op, &rs );
+			sonext=so->s_next;
+			syncprov_drop_psearch( so, 0);
+		}
+		si->si_ops=NULL;
 	}
-	si->si_ops=NULL;
-	ldap_pvt_thread_mutex_unlock( &si->si_ops_mutex );
 	overlay_unregister_control( be, LDAP_CONTROL_SYNC );
 #endif /* SLAP_CONFIG_DELETE */
 
