e1559100eb8e9a664cd68915e5acbf8caa334fa1
Howard Chu <hyc@openldap.org>
2012-12-03 18:00:36 -0800

    Fix typo
    See commit bb36bdcd1c22d1fbc6575452ef5c9112715ab083

--- servers/slapd/back-bdb/idl.c
+++ servers/slapd/back-bdb/idl.c
@@ -1454,7 +1454,7 @@ bdb_idl_sort( ID *ids, ID *tmp )
 			ids[l+1] = ids[j];
 			ids[j] = a;
 			jstack += 2;
-			if (ir-i+1 >= j-1) {
+			if (ir-i+1 >= j-l) {
 				istack[jstack] = ir;
 				istack[jstack-1] = i;
 				ir = j-1;
