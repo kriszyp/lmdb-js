021799aca45f676add543e769fc2426005b7d2c5
Pierangelo Masarati <ando@OpenLDAP.org>
2013-01-06 17:14:22 +0100

    fix slapd.conf name (ITS#7478)

--- servers/slapd/overlays/sssvlv.c
+++ servers/slapd/overlays/sssvlv.c
@@ -1254,7 +1254,7 @@ static ConfigTable sssvlv_cfg[] = {
 		"( OLcfgOvAt:21.2 NAME 'olcSssVlvMaxKeys' "
 			"DESC 'Maximum number of Keys in a Sort request' "
 			"SYNTAX OMsInteger SINGLE-VALUE )", NULL, NULL },
-	{ "sssvlv-maxpercon", "num",
+	{ "sssvlv-maxperconn", "num",
 		2, 2, 0, ARG_INT|ARG_OFFSET,
 			(void *)offsetof(sssvlv_info, svi_max_percon),
 		"( OLcfgOvAt:21.3 NAME 'olcSssVlvMaxPerConn' "
