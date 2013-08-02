96a61b8a077a6a8d3ef77cdcbc9eb32f6a689ceb
Timur Kristóf <venemo@msn.com>
2013-08-02 08:54:58 +0200

    change: comments for example3

--- example3-multiple-transactions.js
+++ example3-multiple-transactions.js
@@ -34,15 +34,18 @@ console.log("txn2: started");
 txn2.putString(dbi, 1, "Ha ha ha");
 console.log("txn2: put other value to key 1");
 
+// txn2 sees the new value immediately
 data = txn2.getString(dbi, 1);
 console.log("-----", "txn2", 1, data);
 
+// txn1 still sees the old value
 data = txn1.getString(dbi, 1);
 console.log("-----", "txn1", 1, data);
 
 txn2.commit();
 console.log("txn2: committed");
 
+// txn1 still sees the old value!
 data = txn1.getString(dbi, 1);
 console.log("-----", "txn1", 1, data);
 
@@ -50,6 +53,7 @@ txn1.reset();
 txn1.renew();
 console.log("rxn1: reset+renewed");
 
+// now txn1 sees the new value
 data = txn1.getString(dbi, 1);
 console.log("-----", "txn1", 1, data);
 
