3c346ded826690d7152c02a48d455f4c03683da6
Timur Kristóf <venemo@msn.com>
2013-08-03 13:49:30 +0200

    add: example for using dupSort with cursor

---
+++ example5-dupsort.js
@@ -0,0 +1,85 @@
+
+var lmdb, env, dbi;
+
+lmdb = require('./build/Release/node-lmdb');
+env = new lmdb.Env();
+env.open({
+    // Path to the environment
+    path: "./testdata",
+    // Maximum number of databases
+    maxDbs: 10
+});
+
+try {
+    // If the database exists, drop it
+    dbi = env.openDbi({
+       name: "example5-dupsort",
+       dupSort: true
+    });
+    dbi.drop();
+}
+catch (err) {}
+dbi = env.openDbi({
+   name: "example5-dupsort",
+   create: true,
+   dupSort: true
+});
+
+console.log("ensured database is empty");
+
+var printFunc = function(key, data) {
+    console.log("---------->  key:", key);
+    console.log("----------> data:", data);
+}
+
+var txn, cursor;
+
+txn = env.beginTxn();
+txn.putNumber(dbi, "hello", 1);
+txn.putNumber(dbi, "apple", 2);
+txn.putNumber(dbi, "orange", 3);
+txn.putNumber(dbi, "apple", 4);
+txn.putNumber(dbi, "hello", 5);
+txn.putNumber(dbi, "appricot", 6);
+txn.putNumber(dbi, "hello", 7);
+txn.commit();
+
+console.log("wrote test values");
+
+txn = env.beginTxn({ readOnly: true });
+cursor = new lmdb.Cursor(txn, dbi);
+
+console.log("goToRange 'banana'");
+cursor.goToRange("banana");
+cursor.getCurrentNumber(printFunc);
+
+console.log("goToNext");
+cursor.goToNext();
+cursor.getCurrentNumber(printFunc);
+
+console.log("goToNext");
+cursor.goToNext();
+cursor.getCurrentNumber(printFunc);
+
+console.log("goToNext");
+cursor.goToNext();
+cursor.getCurrentNumber(printFunc);
+
+console.log("goint past the last item, expecting error");
+try {
+    console.log("goToNext");
+    cursor.goToNext();
+    cursor.getCurrentNumber(printFunc);
+}
+catch (err) {
+    console.log(err);
+}
+
+cursor.close();
+txn.abort();
+dbi.close();
+env.close();
+
+
+
+
