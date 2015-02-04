915c8108fa83973dee075cba56f852d7744d90c2
Howard Chu <hyc@openldap.org>
2015-02-04 05:21:46 +0000

    ITS#8051 use a regexp

--- servers/slapd/back-sock/back-sock.h
+++ servers/slapd/back-sock/back-sock.h
@@ -30,8 +30,8 @@ struct sockinfo {
 	slap_mask_t	si_extensions;
 	slap_mask_t	si_ops;		/* overlay: operations to act on */
 	slap_mask_t	si_resps;	/* overlay: responses to forward */
-	BerVarray	si_suffix;	/* overlay: DN suffixes to match */
-	BerVarray	si_nsuffix;	/* overlay: DN suffixes to match */
+	regex_t	si_dnpat;		/* overlay: DN pattern to match */
+	struct berval 	si_dnpatstr;
 };
 
 #define	SOCK_EXT_BINDDN	1
