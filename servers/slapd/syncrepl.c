2eaf8eb213499a00fea2e9f449fad5d7a4493728
Howard Chu <hyc@openldap.org>
2015-08-29 18:29:32 +0100

    ITS#8231 fix ITS#8042 regression

--- servers/slapd/syncrepl.c
+++ servers/slapd/syncrepl.c
@@ -2769,6 +2769,9 @@ presentlist_find(
 	Avlnode **a2 = (Avlnode **)av;
 	unsigned short s;
 
+	if (!av)
+		return NULL;
+
 	memcpy(&s, val->bv_val, 2);
 	return avl_find( a2[s], val->bv_val+2, syncuuid_cmp );
 #else
