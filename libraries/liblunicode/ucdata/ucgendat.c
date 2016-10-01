cc99da182f53d3d4f3874703643b277773717af3
Zebediah Figura <z.figura12@gmail.com>
2016-09-30 21:26:59 -0500

    ITS#8508 - Allow ucgendat.c to recognize title-case characters even if they do not have lower-case equivalents
    I, Zebediah Figura, hereby place the following modifications to OpenLDAP Software (and only these modifications) into the public domain. Hence, these modifications may be freely used and/or redistributed for any purpose with or without attribution and/or other notice.

--- libraries/liblunicode/ucdata/ucgendat.c
+++ libraries/liblunicode/ucdata/ucgendat.c
@@ -536,6 +536,13 @@ add_title(ac_uint4 code)
      */
     cases[2] = code;
 
+    /*
+     * If the upper case character is not present, then make it the same as
+     * the title case.
+     */
+    if (cases[0] == 0)
+      cases[0] = code;
+
     if (title_used == title_size) {
         if (title_size == 0)
           title = (_case_t *) malloc(sizeof(_case_t) << 3);
@@ -818,7 +825,7 @@ read_cdata(FILE *in)
 {
     ac_uint4 i, lineno, skip, code, ccl_code;
     short wnum, neg, number[2], compat;
-    char line[512], *s, *e;
+    char line[512], *s, *e, *first_prop;
 
     lineno = skip = 0;
     while (fgets(line, sizeof(line), in)) {
@@ -962,6 +969,8 @@ read_cdata(FILE *in)
               i++;
         }
         for (e = s; *e && *e != ';'; e++) ;
+
+        first_prop = s;
     
         ordered_range_insert(code, s, e - s);
 
@@ -1109,7 +1118,7 @@ read_cdata(FILE *in)
             if (*s == ';')
               s++;
         }
-        if (cases[0] && cases[1])
+        if (!strncmp(first_prop,"Lt",2) && (cases[0] || cases[1]))
           /*
            * Add the upper and lower mappings for a title case character.
            */
