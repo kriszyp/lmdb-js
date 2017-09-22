35a880c53ed0d06b9d0010c9a3ddca04f66dfa9c
Quanah Gibson-Mount <quanah@openldap.org>
2017-09-22 14:25:20 -0700

    ITS#8687 - EGD is disabled by default in OpenSSL 1.1. We need to comment out this block if it is not detected. Particularly affects cross compilation.

--- libraries/libldap/tls_o.c
+++ libraries/libldap/tls_o.c
@@ -1309,11 +1309,13 @@ tlso_seed_PRNG( const char *randfile )
 		 * The fact is that when $HOME is NULL, .rnd is used.
 		 */
 		randfile = RAND_file_name( buffer, sizeof( buffer ) );
-
-	} else if (RAND_egd(randfile) > 0) {
+	}
+#ifndef OPENSSL_NO_EGD
+	else if (RAND_egd(randfile) > 0) {
 		/* EGD socket */
 		return 0;
 	}
+#endif
 
 	if (randfile == NULL) {
 		Debug( LDAP_DEBUG_ANY,
