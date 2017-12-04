f82ca15a1862749e13a989599d7f4c52c7648957
Howard Chu <hyc@openldap.org>
2017-12-04 15:59:33 +0000

    ITS#8782 plug memleaks in cancel

--- libraries/libldap/cancel.c
+++ libraries/libldap/cancel.c
@@ -42,14 +42,14 @@ ldap_cancel(
 	int		*msgidp )
 {
 	BerElement *cancelidber = NULL;
-	struct berval *cancelidvalp = NULL;
+	struct berval cancelidvalp = { 0, NULL };
 	int rc;
 
 	cancelidber = ber_alloc_t( LBER_USE_DER );
 	ber_printf( cancelidber, "{i}", cancelid );
-	ber_flatten( cancelidber, &cancelidvalp );
+	ber_flatten2( cancelidber, &cancelidvalp, 0 );
 	rc = ldap_extended_operation( ld, LDAP_EXOP_CANCEL,
-		cancelidvalp, sctrls, cctrls, msgidp );
+		&cancelidvalp, sctrls, cctrls, msgidp );
 	ber_free( cancelidber, 1 );
 	return rc;
 }
@@ -62,14 +62,14 @@ ldap_cancel_s(
 	LDAPControl	**cctrls )
 {
 	BerElement *cancelidber = NULL;
-	struct berval *cancelidvalp = NULL;
+	struct berval cancelidvalp = { 0, NULL };
 	int rc;
 
 	cancelidber = ber_alloc_t( LBER_USE_DER );
 	ber_printf( cancelidber, "{i}", cancelid );
-	ber_flatten( cancelidber, &cancelidvalp );
+	ber_flatten2( cancelidber, &cancelidvalp, 0 );
 	rc = ldap_extended_operation_s( ld, LDAP_EXOP_CANCEL,
-		cancelidvalp, sctrls, cctrls, NULL, NULL );
+		&cancelidvalp, sctrls, cctrls, NULL, NULL );
 	ber_free( cancelidber, 1 );
 	return rc;
 }
