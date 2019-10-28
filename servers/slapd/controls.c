230b4696691b67a84bc8309775c789a40fdcc4ff
Howard Chu <hyc@openldap.org>
2019-10-28 19:01:36 +0000

    ITS#9100 relax domainScope check for absent value

--- servers/slapd/controls.c
+++ servers/slapd/controls.c
@@ -1714,7 +1714,11 @@ static int parseDomainScope (
 		return LDAP_PROTOCOL_ERROR;
 	}
 
-	if ( !BER_BVISNULL( &ctrl->ldctl_value )) {
+	/* this should be checking BVISNULL, but M$ clients are broken
+	 * and include the value even though the M$ spec says it must be
+	 * omitted. ITS#9100.
+	 */
+	if ( !BER_BVISEMPTY( &ctrl->ldctl_value )) {
 		rs->sr_text = "domainScope control value not absent";
 		return LDAP_PROTOCOL_ERROR;
 	}
