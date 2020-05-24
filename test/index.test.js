cb7f35e4d2f6c893b3e966f64c6354fd4e6cc439
D4 <d4@thegrid.io>
2020-05-23 18:54:40 -0700

    electron scripts

--- test/index.test.js
+++ test/index.test.js
@@ -289,7 +289,7 @@ describe('Node.js LMDB Bindings', function() {
       txn.abort();
       dbi.close();
     });
-    it.only('will create a database and back it up', function (done) {
+    it('will create a database and back it up', function (done) {
       var txn = env.beginTxn();
       var dbi = env.openDbi({
         name: 'backup',
