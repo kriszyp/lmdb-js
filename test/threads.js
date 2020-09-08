d242ff6fec103d3455e6889b223eaaf5ea1f88a1
Kris Zyp <kriszyp@gmail.com>
2020-09-07 18:12:24 -0600

    Fix parent thread reference

--- test/threads.js
+++ test/threads.js
@@ -88,7 +88,7 @@ if (isMainThread) {
       var value = txn.getBinary(dbi, msg.key);
       
       if (value === null) {
-        parent.postMessage("");
+        parentPort.postMessage("");
       } else {
         parentPort.postMessage(value.toString('hex'));
       }
