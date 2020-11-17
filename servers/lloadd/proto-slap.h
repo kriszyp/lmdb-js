mistotebe/openldap

Option for response handling

servers/lloadd/proto-slap.h
@@ -281,6 +281,7 @@ LDAP_SLAPD_V (ber_len_t) sockbuf_max_incoming;
 LDAP_SLAPD_V (ber_len_t) sockbuf_max_incoming_auth;
 LDAP_SLAPD_V (int)		slap_conn_max_pending;
 LDAP_SLAPD_V (int)		slap_conn_max_pending_auth;
+LDAP_SLAPD_V (int)      slap_conn_max_responses_per_cycle;
 
 LDAP_SLAPD_V (slap_mask_t)	global_allows;
 LDAP_SLAPD_V (slap_mask_t)	global_disallows;

kriszyp/lmdb-js

servers/lloadd/proto-slap.h
@@ -206,6 +206,7 @@ LDAP_SLAPD_F (int) slap_zn_wunlock( void *, void * );
 
 LDAP_SLAPD_V (ber_len_t) sockbuf_max_incoming;
 LDAP_SLAPD_V (ber_len_t) sockbuf_max_incoming_auth;
+LDAP_SLAPD_V (int) slap_conn_max_pdus_per_cycle;
 
 LDAP_SLAPD_V (slap_mask_t) global_allows;
 LDAP_SLAPD_V (slap_mask_t) global_disallows;