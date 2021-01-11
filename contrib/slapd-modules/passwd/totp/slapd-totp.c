a0f2b003d59f89dc9d5f412ec75ce45fa3d022e1
Quanah Gibson-Mount <quanah@openldap.org>
2021-01-11 19:30:43 +0000

    Happy New Year!

--- contrib/slapd-modules/passwd/totp/slapd-totp.c
+++ contrib/slapd-modules/passwd/totp/slapd-totp.c
@@ -2,7 +2,7 @@
 /* $OpenLDAP$ */
 /* This work is part of OpenLDAP Software <http://www.openldap.org/>.
  *
- * Copyright 2015-2020 The OpenLDAP Foundation.
+ * Copyright 2015-2021 The OpenLDAP Foundation.
  * Portions Copyright 2015 by Howard Chu, Symas Corp.
  * All rights reserved.
  *
