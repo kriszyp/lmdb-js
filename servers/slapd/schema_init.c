9badb73425a67768c09bcaed1a9c26c684af6c30
Howard Chu <hyc@openldap.org>
2021-02-06 20:52:06 +0000

    ITS#9454 fix issuerAndThisUpdateCheck

--- servers/slapd/schema_init.c
+++ servers/slapd/schema_init.c
@@ -3900,6 +3900,8 @@ issuerAndThisUpdateCheck(
 					break;
 				}
 			}
+			if ( tu->bv_len < STRLENOF("YYYYmmddHHmmssZ") ) return LDAP_INVALID_SYNTAX;
+
 			x.bv_val += tu->bv_len + 1;
 			x.bv_len -= tu->bv_len + 1;
 
