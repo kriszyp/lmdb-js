464f1c5cca8b007ff3d196190eb8230ae488d845
Quanah Gibson-Mount <quanah@openldap.org>
2021-02-10 20:27:34 +0000

    ITS#9464 - Drop unused AC variables

--- tests/scripts/conf.sh
+++ tests/scripts/conf.sh
@@ -34,12 +34,10 @@ sed -e "s/@BACKEND@/${BACKEND}/"			\
 	-e "s/^#${AC_asyncmeta}#//"			\
 	-e "s/^#${AC_relay}#//"				\
 	-e "s/^#${AC_sql}#//"				\
-		-e "s/^#${RDBMS}#//"			\
+	-e "s/^#${RDBMS}#//"			\
 	-e "s/^#${AC_accesslog}#//"			\
-	-e "s/^#${AC_autoca}#//"			\
 	-e "s/^#${AC_dds}#//"				\
 	-e "s/^#${AC_dynlist}#//"			\
-	-e "s/^#${AC_memberof}#//"			\
 	-e "s/^#${AC_pcache}#//"			\
 	-e "s/^#${AC_ppolicy}#//"			\
 	-e "s/^#${AC_refint}#//"			\
@@ -49,7 +47,6 @@ sed -e "s/@BACKEND@/${BACKEND}/"			\
 	-e "s/^#${AC_translucent}#//"			\
 	-e "s/^#${AC_unique}#//"			\
 	-e "s/^#${AC_valsort}#//"			\
-	-e "s/^#${AC_lloadd}#//"			\
 	-e "s/^#${INDEXDB}#//"				\
 	-e "s/^#${MAINDB}#//"				\
 	-e "s/^#${SASL}#//"				\
