f8cc2d211c0f53d8323f670a26bc82150a7f0207
Kris Zyp <kriszyp@gmail.com>
2021-08-20 21:41:16 -0600

    Try using double for sqrt

--- dependencies/lmdb/libraries/liblmdb/mdb.c
+++ dependencies/lmdb/libraries/liblmdb/mdb.c
@@ -3983,8 +3983,7 @@ mdb_page_flush(MDB_txn *txn, int keep)
 		size_t file_size = GetFileSize(fd, &file_high);
 		file_size += (size_t) file_high << 32;
 		if (pgno * psize >= file_size) {
-			auto psModifier = sqrt(pgno + 128);
-			//fprintf(stderr, "ps %d ", psModifier);
+			double psModifier = sqrt(pgno + 128);
 			file_size = ((size_t) ((1.04 + 16 / psModifier) * pgno * psize / 0x40000 + 1)) * 0x40000;
 			LONG high_position = file_size >> 32;
 			if (SetFilePointer(fd, file_size & 0xffffffff, &high_position, FILE_BEGIN) == INVALID_SET_FILE_POINTER) {
