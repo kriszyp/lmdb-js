06d17620163231fd53023e1fd8074af90cada775
Kris Zyp <kriszyp@gmail.com>
2022-10-27 08:47:19 -0600

    Use ArrayBuffer references to update compression

--- src/compression.cpp
+++ src/compression.cpp
@@ -16,7 +16,7 @@ Compression::Compression(const CallbackInfo& info) : ObjectWrap<Compression>(inf
 				throwError(info.Env(), "Dictionary must be a buffer");
 				return;
 			}
-			napi_get_typedarray_info(info.Env(), dictionaryOption, nullptr, &dictSize, (void**) &dictionary, nullptr, nullptr);
+			napi_get_buffer_info(info.Env(), dictionaryOption, (void**) &dictionary, &dictSize);
 			dictSize = (dictSize >> 3) << 3; // make sure it is word-aligned
 		}
 		auto thresholdOption = info[0].As<Object>().Get("threshold");
@@ -34,10 +34,12 @@ Compression::Compression(const CallbackInfo& info) : ObjectWrap<Compression>(inf
 }
 
 Napi::Value Compression::setBuffer(const CallbackInfo& info) {
-	napi_get_typedarray_info(info.Env(), info[0], nullptr, nullptr, (void**) &this->decompressTarget, nullptr, nullptr);
+	size_t length;
+	napi_get_arraybuffer_info(info.Env(), info[0], (void**) &this->decompressTarget, &length);
 	this->decompressSize = info[1].As<Number>();
-	napi_get_typedarray_info(info.Env(), info[2], nullptr, nullptr, (void**) &this->dictionary, nullptr, nullptr);
 	this->dictionarySize = info[3].As<Number>();
+	this->decompressTarget += this->dictionarySize;
+	napi_get_arraybuffer_info(info.Env(), info[2], (void**) &this->dictionary, &length);
 	return info.Env().Undefined();
 }
 void Compression::decompress(MDB_val& data, bool &isValid, bool canAllocate) {
