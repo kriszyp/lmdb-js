06b625afdd34111ba0354bc7699d53202800fbc8
Kris Zyp <kriszyp@gmail.com>
2023-02-22 15:36:46 -0700

    Propagate errors on exec failures

--- bin/download-prebuilds.js
+++ bin/download-prebuilds.js
@@ -4,9 +4,12 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { exec } from 'child_process';
 
-exec('download-msgpackr-prebuilds', (error, stdout, stderr) => {
+exec('download-msgpackr-prebuilds', propagateOutput);
+process.chdir(fileURLToPath(dirname(dirname(import.meta.url))));
+exec('prebuildify-ci download', propagateOutput);
+function propagateOutput(error, stdout, stderr) {
 	console.error(stderr);
 	console.log(stdout);
-});
-process.chdir(fileURLToPath(dirname(dirname(import.meta.url))));
-exec('prebuildify-ci download');
+	if (error?.code)
+		process.exit(error.code);
+}
