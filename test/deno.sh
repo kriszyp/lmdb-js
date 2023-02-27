dabf4d030d308ba7daa7daca3a6a94ac2da5ba2d
Kris Zyp <kriszyp@gmail.com>
2023-02-27 08:37:04 -0700

    Try newer deps

--- test/deno.sh
+++ test/deno.sh
@@ -1,1 +1,1 @@
-deno run --unstable --allow-env --allow-read --allow-write --allow-ffi test/deno.ts
+deno run --reload --unstable --allow-env --allow-read --allow-write --allow-ffi test/deno.ts
--- write.js
+++ write.js
@@ -119,7 +119,11 @@ export function addWriteMethods(LMDBStore, { env, fixedBuffer, resetReadTxn, use
 				enqueuedEventTurnBatch = queueTask(() => {
 					try {
 						for (let i = 0, l = beforeCommitCallbacks.length; i < l; i++) {
-							beforeCommitCallbacks[i]();
+							try {
+								beforeCommitCallbacks[i]();
+							} catch(error) {
+								console.error('In beforecommit callback', error);
+							}
 						}
 					} catch(error) {
 						console.error(error);
@@ -497,7 +501,11 @@ export function addWriteMethods(LMDBStore, { env, fixedBuffer, resetReadTxn, use
 	}
 	function afterCommit(txnId) {
 		for (let i = 0, l = afterCommitCallbacks.length; i < l; i++) {
-			afterCommitCallbacks[i]({ next: uncommittedResolution, last: unwrittenResolution, txnId });
+			try {
+				afterCommitCallbacks[i]({next: uncommittedResolution, last: unwrittenResolution, txnId});
+			} catch(error) {
+				console.error('In aftercommit callback', error);
+			}
 		}
 	}
 	async function executeTxnCallbacks() {
